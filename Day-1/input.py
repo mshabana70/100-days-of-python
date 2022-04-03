bdfa082470a2a426aa96506b53e2cd6ffd147ea7
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-03 17:50:05 -0400

    set input value to variable

--- Day-1/input.py
+++ Day-1/input.py
@@ -3,4 +3,12 @@
 
 # This takes in a user input
 # Lets try to execute commands on the input
-print("Hello " + input("What is your name?"))
+#print("Hello " + input("What is your name?"))
+
+# Lets try to store the returned user input with
+# a variable
+
+name = input("Please give us a word or phrase: ")
+nameLen = len(name)
+print("The length of '" + name + "' is ")
+print(nameLen)
