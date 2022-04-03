34324d4df337aa1a749b3f4286458471cf51ce5b
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-03 16:31:22 -0400

    exercise 1 interactive challenege

---
+++ Day-1/exercise-1.py
@@ -0,0 +1,9 @@
+# Write a program that prints out the following output
+
+# Day 1 - Python Print Function
+# The function is declared like this
+# print('what to print')
+
+# My Attempt
+
+print("Day 1 - Python Print Function\nThe function is declared like this\nprint('what to print')")
