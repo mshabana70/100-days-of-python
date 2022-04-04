f3b45698fb7d1622d8c26b4fa759926de232bcba
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-04 19:33:58 -0400

    Integer data type

--- Day-2/dataTypes.py
+++ Day-2/dataTypes.py
@@ -7,3 +7,10 @@ string = "Hello"
 # A string is a 'string' of characters
 print(string[0]) # Returns 'H'
 print(string[2]) # Returns 'l'
+
+print(string[len(string) - 1]) # Returns last index "o"
+
+# Integer
+bigInt = 123_456_789 # The same as 123,456,789
+
+print(bigInt)
