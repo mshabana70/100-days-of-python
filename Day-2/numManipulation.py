2ab9029b2032a73dc2f469e10c92349a75d5845f
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-04 20:41:11 -0400

    number manipulation

---
+++ Day-2/numManipulation.py
@@ -0,0 +1,12 @@
+# Number Manipulation and F String
+
+# If you want to round a num to the nearest whole integer
+expression = 8 / 3 # 2.666666665
+print(round(expression))
+
+# To round to a specific precision
+print(round(expression, 2)) # rounds to 2 decimal places
+
+# Floor Division
+expressionFloor = 8 // 3 # returns an integer, rounds down
+print(expressionFloor, type(expressionFloor))
