a474ebd65336be8f8cae34211ecfed7851bfb8ad
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-04 20:44:12 -0400

    F strings in python

--- Day-2/numManipulation.py
+++ Day-2/numManipulation.py
@@ -10,3 +10,13 @@ print(round(expression, 2)) # rounds to 2 decimal places
 # Floor Division
 expressionFloor = 8 // 3 # returns an integer, rounds down
 print(expressionFloor, type(expressionFloor))
+
+# F Strings
+#
+# Allows for multiple different data types to be returned 
+# in a string more easily
+score = 87
+print(f'Your score is {score}')
+score2 = 93
+print(f'Your score before studyin was {score}, after studying it was {score2}')
+
