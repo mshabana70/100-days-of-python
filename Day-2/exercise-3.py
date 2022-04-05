987e92e4e6df6a8c3a055665f8f9d6c369a575fc
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-04 20:26:39 -0400

    python exercise 3 - Life in Weeks

---
+++ Day-2/exercise-3.py
@@ -0,0 +1,24 @@
+# Life in Weeks
+
+# Create a program using maths and f-Strings that 
+# tells us how many days, weeks, months we have left 
+# if we live until 90 years old.
+#
+# It will take your current age as the input and output 
+# a message with our time left in this format:
+#
+# You have x days, y weeks, and z months left.
+#
+# Where x, y and z are replaced with the actual calculated 
+# numbers.
+#
+#
+# Example Input: How old are you in years?
+#
+# 56
+#
+#
+# Example Output:
+# 
+# You have 12410 days, 1768 weeks, and 408 months left.
+
