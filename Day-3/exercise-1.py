55e6ecd3430b8c171bdf8a1cc75a3d4663515073
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-05 19:11:50 -0400

    exercise 1 - Even or Odd complete

--- Day-3/exercise-1.py
+++ Day-3/exercise-1.py
@@ -12,3 +12,15 @@
 #
 # 29.5 is not a whole number, it has decimal places. Therefore there is a remainder 
 # of 0.5, so the division is not clean.
+
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+if (number % 2 == 0):
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
+
