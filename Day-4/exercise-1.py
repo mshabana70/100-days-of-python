eb67673f7072bd811c7c7ff11b0a011d33567446
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-06 18:56:18 -0400

    fixed solution and included random floats

--- Day-4/exercise-1.py
+++ Day-4/exercise-1.py
@@ -36,20 +36,16 @@ randomFloat = random.random()
 print(randomFloat)
 # returns random float 0.0 to 1.0
 
-# 
+# Generate random float between 0 - 5
+randomFloat *= 5 # [0.0 - 1.0] * 5 = [0.0 - 5.0]
+print(randomFloat) 
 
 # Lets solve the exercise here
 #Remember to use the random module
 #Hint: Remember to import the random module here at the top of the file. 🎲
 import random
-# 🚨 Don't change the code below 👇
-test_seed = int(input("Create a seed number: "))
-random.seed(test_seed)
- # 🚨 Don't change the code above 👆 It's only for testing your code.
 	 
 #Write the rest of your code below this line 👇
-
-
 flipResult = random.randint(0, 1)
 
 if (flipResult):
