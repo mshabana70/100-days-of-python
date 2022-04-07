64391fee7762de94e110a52c5c9c119a8a1b4d42
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-07 17:15:56 -0400

    Exercise 1 - Average Height Solution complete

--- Day-5/exercise-1.py
+++ Day-5/exercise-1.py
@@ -37,6 +37,12 @@ for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
 # 🚨 Don't change the code above 👆
 
-
 #Write your code below this row 👇
+sum_of_heights = 0
 
+for height in student_heights:
+    sum_of_heights += height
+
+avg_height_rounded = round((sum_of_heights / len(student_heights)))
+
+print(avg_height_rounded)
