233ad7eefe286800a32a21f34b4f8a6e0d0de1dc
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-07 17:47:58 -0400

    Exercise 4 - FizzBuzz Solution complete

--- Day-5/exercise-4.py
+++ Day-5/exercise-4.py
@@ -33,3 +33,12 @@
 # FizzBuzz
 
 # My implementation
+for i in range(1, 101):
+    if (i % 3 == 0 and i % 5 == 0):
+        print("FizzBuzz")
+    elif (i % 3 == 0):
+        print("Fizz")
+    elif (i % 5 == 0):
+        print("Buzz")
+    else:
+        print(i)
