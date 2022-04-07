c850de90b40faebc843a742900a2fa45609caa08
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-06 21:49:15 -0400

    refactored code

--- Day-4/project/rock-paper-scissors.py
+++ Day-4/project/rock-paper-scissors.py
@@ -37,35 +37,25 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+game_images = [rock, paper, scissors]
 
-# Return User decision
-if (userChoice == "1"):
-    print(rock)
-elif (userChoice == "2"):
-    print(paper)
-elif (userChoice == "3"):
-    print(scissors)
-else:
+# Return User decision and computer decision
+if (int(userChoice) > 3 or int(userChoice) < 0):
     print("Please select one of the given integers.")
+else:
+    print("User Choice: ")
+    print(game_images[int(userChoice) - 1])
+    print("Computer choice: ")
+    print(game_images[randomInteger - 1])
 
-# Conditionals
-
+# Dictionary values for computer (messing with dictionaries)
 dictionary = {
     1: "rock",
     2: "paper",
     3: "scissors"
 }
 
-# Return Computer Decision
-if (dictionary[randomInteger] == "rock"):
-    print(rock)
-elif (dictionary[randomInteger] == "paper"):
-    print(paper)
-else:
-    print(scissors)
-
-
-# Determine a winner
+# Determine a winner with conditionals
 if (userChoice == str(randomInteger)):
     print("It is a draw!")
 elif (userChoice == "1"):
