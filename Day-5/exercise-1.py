d38c60a9ed39563cf26a381c475bec4237c3578a
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-07 17:08:08 -0400

    example inputs/outputs and starting code

--- Day-5/exercise-1.py
+++ Day-5/exercise-1.py
@@ -21,3 +21,22 @@
 # Important You should not use the sum() or len() functions in your answer. 
 # You should try to replicate their functionality using what you have learnt 
 # about for loops.
+# 
+# Example Input:
+# 
+# 156 178 165 171 187
+#
+# Example Output:
+#
+# 171
+
+
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+
