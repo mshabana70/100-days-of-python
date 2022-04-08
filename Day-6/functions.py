8611fd90ae9b56b2ab7b657173ae7eed296a2e77
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-08 18:19:22 -0400

    Functions in python

---
+++ Day-6/functions.py
@@ -0,0 +1,16 @@
+# Day 6 - Functions
+# https://docs.python.org/3/library/functions.html
+
+# Examples of built-in functions
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+# What if we want to make our own functions
+# first start with keyword: def
+def my_function():
+    print("Hello")
+    print("Bye")
+
+# Nothing will happen until we call the function
+my_function()
