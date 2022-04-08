67d15ec96dbd593afb136167b7dbd35b60e01248
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-07 20:21:22 -0400

    password generator overview

--- Day-5/project/password-generator.py
+++ Day-5/project/password-generator.py
@@ -1,3 +1,6 @@
 # Day 5 Project: Create a Password Generator
 #
+# Write a program to takes in user input for the length of password,
+# number of symbols in the password and number of numbers in the password.
+#
 # 
