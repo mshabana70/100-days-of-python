731ab174828427c45376e9bc9989c1654f0694aa
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-07 20:46:39 -0400

    Day 5 password generator preexercise values

--- Day-5/project/password-generator.py
+++ Day-5/project/password-generator.py
@@ -3,4 +3,25 @@
 # Write a program to takes in user input for the length of password,
 # number of symbols in the password and number of numbers in the password.
 #
-# 
+
+# Values in lists
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C",
+"D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+"T", "U", "V", "W", "X", "Y", "Z" ]
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password? \n"))
+nr_symbols = int(input("How many symbols would you like in your password? \n"))
+nr_numbers = int(input("How many numbers would you like in your password? \n"))
+
+# Easy version
+# fghf^&23
+final_password = ""
+for i in letters:
+
+
+
+
