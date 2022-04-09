1e204a4b555e371de4a5f464f473a780f2bc7ea8
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-09 18:29:18 -0400

    Step 2 - overview

--- Day-7/project/hangman.py
+++ Day-7/project/hangman.py
@@ -23,17 +23,37 @@ word_list = ["ardvark", "baboon", "camel"]
 #target_word = word_list[randomInt]
 target_word = random.choice(word_list)
 
+print(f"Psssst... The target word is {target_word}") # For testing
+
 # Ask for User input on a guess letter and assign it to guess
 guess = input("Please guess a letter: ").lower()
 
 # Check if guessed letter is in target_word (check each letter)
-for letter in target_word:
-    if guess == letter:
-        print("Right")
-    else:
-        print("Wrong")
+# for letter in target_word:
+#     if guess == letter:
+#         print("Right")
+#     else:
+#         print("Wrong")
 
+# Step 2
 
+# TODO-1 - Create an empty list called display.
+# For each letter in the target_word, add an "_" to 
+# 'display'.
+# So if the target_word was "apple", display should be
+# ["_", "_", "_", "_", "_"] with 5 "_" representing each 
+# letter to guess.
+
+# TODO-2 - Loop through each position in the target_word.
+# If the letter at that position matches 'guess' then reveal
+# that letter in the display at that position.
+# e.g. if the the user guessed "p" and the target_word was "apple",
+# then the display should be ["_", "p", "p", "_", "_"].
+
+# TODO-3 - Print 'display' and you should see the guessed letter
+# in the correct position and every other letter replace with "_".
+# Hint - Dont worry about getting the user to guess the next letter.
+# We'll tackle that in step 3.
 
 
 
