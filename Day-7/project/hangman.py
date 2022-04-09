7052396562637c0226a171f0bc961cdb3f80bfdc
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-09 18:01:35 -0400

    Flowcharts of hangman project

---
+++ Day-7/project/hangman.py
@@ -0,0 +1,17 @@
+# Day 7 Project - Hangman
+#
+# Build a program that works like the popular hangman game
+
+# Step 1 of project
+
+# TODO-1 - Randomly choose a word from the word_list and 
+# assign it to the variable called target_word
+
+# TODO-2 - Ask the user to guess a letter and assign their
+# answer to a variable called guess. Make guess lowercase
+
+# TODO-3 - Check if the letter the user guessed (guess) is 
+# one of the letters in the chosen_word
+
+word_list = ["ardvark", "baboon", "camel"]
+
