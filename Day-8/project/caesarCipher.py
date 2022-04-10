1fe34aa0c3c1f248dd79d2af819c950921e9fa81
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-10 19:31:47 -0400

    decrypt function

--- Day-8/project/caesarCipher.py
+++ Day-8/project/caesarCipher.py
@@ -49,8 +49,35 @@ def caesar_cipher(message, shift, direction):
     new_message = new_message.join(message_list) # join message back to a string for readablility
     print(f"The {direction}ed message is {new_message}") # print back new message
 
-caesar_cipher(text, shift, direction)
+#caesar_cipher(text, shift, direction)
 
 
+# Angela's Solution
+
+# Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = letters_list.index(letter)
+        new_position = position + shift_amount
+        new_letter = letters_list[new_position % (len(letters_list))]
+        cipher_text += new_letter
+    print(f"The encoded text is {cipher_text}")
+
+#encrypt(plain_text = text, shift_amount = shift)
+
+# Create a function called 'decrypt'
+def decrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = letters_list.index(letter)
+        new_position = position - shift_amount
+        new_letter = letters_list[new_position % (len(letters_list))]
+        cipher_text += new_letter
+    print(f"The decoded text is {cipher_text}")
+
+# decrypt(plain_text = text, shift_amount = shift)
+
+# Create a function that 
         
 
