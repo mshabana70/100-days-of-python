3354398f0ee572a4e31360748ca2e1cc3a795299
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-10 16:15:08 -0400

    reviewing functions

---
+++ Day-8/functions-review.py
@@ -0,0 +1,13 @@
+# Day 8 - Review
+# 
+# Create a function called greet().
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+# Create greet()
+def greet():
+    print("Hello from greet!")
+    print("It is a pleasure to see you")
+    print("Wish you a great day!")
+
+greet()
