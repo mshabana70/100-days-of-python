aaf98f8ed9fdd11b35c121f75e935db80c3707ad
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-10 19:10:54 -0400

    commented code

--- Day-8/project/caesarCipher.py
+++ Day-8/project/caesarCipher.py
@@ -19,24 +19,35 @@ shift = int(input("Type the shift number:\n"))
 def caesar_cipher(message, shift, direction):
     index = 0
     new_message = ""
-    message_list = list(message)
+    message_list = list(message) # set message to a list
+
+    # Iterate through the message_list
     for i in range(len(message_list)):
+
+        # If a letter in message is in the alphabet (so it ignores spaces and symbols)
         if message[i] in letters_list:
+
+            # get index of letter in the letter_list (so we can shift from this point to the new encoded letter)
             index = letters_list.index(message_list[i])
+
+            # check the direction (encode or decode)
             if (direction == 'encode'):
-                if ((index + shift) > 25):
-                    index = (index + shift) - 26
+
+                # If the shift excedes the size of our list, start back at the beginning (loop back to the front)
+                if ((index + shift) > (len(letters_list) - 1)):
+                    index = (index + shift) - (len(letters_list)) # subtract length from new index to start from beginning
                 else: 
                     index += shift
-                message_list[i] = letters_list[index]
+                message_list[i] = letters_list[index] # set message letter to the new encoded letter (post shift)
             else:
+                # If the shift excedes the size of our list, start back at the beginning (loop back to the front)
                 if ((index - shift) < 0):
-                    index = (index - shift) + 26
+                    index = (index - shift) + (len(letters_list)) # add length from new index to start from the end
                 else: 
                     index -= shift
-                message_list[i] = letters_list[index]
-    new_message = new_message.join(message_list)
-    print(f"The {direction}ed message is {new_message}")
+                message_list[i] = letters_list[index] # set message letter to the new decoded letter (post shift)
+    new_message = new_message.join(message_list) # join message back to a string for readablility
+    print(f"The {direction}ed message is {new_message}") # print back new message
 
 caesar_cipher(text, shift, direction)
 
