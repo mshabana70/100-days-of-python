503105ef4753ac7a430ea5d94fee49d28f90d050
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-12 19:18:43 -0400

    Function with return value

--- Day-10/functions.py
+++ Day-10/functions.py
@@ -1,3 +1,11 @@
 # Day 10 - Functions with outputs
 #
-#
+# Test some functions with outputs
+
+def format_name(f_name, l_name):
+    full_name = ""
+    full_name = f_name.title() + " " + l_name.title()
+    return f"{full_name}"
+
+# Call our function and see what the returned value is
+print(format_name("MAHMOUD", "SHABANA")) # Output: Mahmoud Shabana
