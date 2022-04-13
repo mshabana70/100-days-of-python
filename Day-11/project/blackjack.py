3b21ac128f5cc691719662187d86e2af5cceebcc
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-13 17:57:37 -0400

    Blackjack capstone project overview

---
+++ Day-11/project/blackjack.py
@@ -0,0 +1,12 @@
+# Day 11 Project - Blackjack Game (Capstone)
+#
+# Create a black jack game in python, where you play against
+# the dealer. You are given two cards at random, with the value of 
+# of cards being what the number is (for number cards) and for face 
+# cards it is a value of 10.
+#
+# You place a bet, dealer deals the cards. You can call on your hand
+# if you want or just fold and end the game. If you call and exceed 21, 
+# you lose, if you are closer to 21 than the dealer, you win!
+
+
