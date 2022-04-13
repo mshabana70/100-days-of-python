ed05b2fa947018fb1f2104dbc64b8f3d10ccacd6
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-12 20:05:38 -0400

    add, subtract, multiply, divide functions

---
+++ Day-10/project/calculator.py
@@ -0,0 +1,27 @@
+# Day 10 Project - Calculator with functions
+#
+# Create a classic calculator program using functions
+# with return keywords
+
+
+# Calculator Functionality
+
+# Add
+def add(n1, n2):
+    """Returns the sum of two integers"""
+    return n1 + n2
+
+# Subtract
+def subtract(n1, n2):
+    """Returns the difference of two integers"""
+    return n1 - n2
+
+# Multiply
+def multiply(n1, n2):
+    """Returns the product of two integers"""
+    return n1 * n2
+
+# Divide
+def divide(n1, n2):
+    """Returns the quotient of two integers"""
+    return n1 / n2
