a4e05578011eeba9e4b5592371f98517c392ddbf
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-12 20:15:43 -0400

    used dictionary to call functions based on user input

--- Day-10/project/calculator.py
+++ Day-10/project/calculator.py
@@ -38,3 +38,17 @@ operations = {
 #function(2, 3) # Same as calling multiply(2, 3)
 
 # Ask for user input
+num1 = int(input("What is the first number?: "))
+num2 = int(input("What is the second number?: "))
+
+# Loop through dictionary and show what symbols are available to the user
+print("Here are the operations available to you.")
+for operator in operations:
+    print(operator, "\n") # +, -, *, /
+
+operation_symbol = input("Pick an operation from the line above: ")
+
+# Take operation_symbol and pick the value (function) associated to it
+function = operations[operation_symbol]
+
+print(f"{num1} {operation_symbol} {num2} = {function(num1, num2)}")
