d800d4addc6ea68eb217d0f09b9a33e8f6dd70b3
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-14 16:27:34 -0400

    comments for clarification

--- Day-12/scope.py
+++ Day-12/scope.py
@@ -1,11 +1,11 @@
 # Day 12 - Scope (Local - Global)
 
 # Scope
-enemies = 1
+enemies = 1 # Global
 
 def increase_enemies():
-    enemies = 2
-    print(f"enemies inside the function: {enemies}")
+    enemies = 2 # Local (can only be referenced in function scope)
+    print(f"enemies inside the function: {enemies}") # Output: 2
 
 increase_enemies()
-print(f"enemies outside the function: {enemies}")
+print(f"enemies outside the function: {enemies}") # Output: 1
