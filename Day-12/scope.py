5209b3b9727a340759ff769506a14bf57b16675c
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-14 16:26:31 -0400

    example of local and global scope

--- Day-12/scope.py
+++ Day-12/scope.py
@@ -1,1 +1,11 @@
 # Day 12 - Scope (Local - Global)
+
+# Scope
+enemies = 1
+
+def increase_enemies():
+    enemies = 2
+    print(f"enemies inside the function: {enemies}")
+
+increase_enemies()
+print(f"enemies outside the function: {enemies}")
