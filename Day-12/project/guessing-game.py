57781fbfcbbfe45294fecce371b3784802db2917
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-14 17:01:20 -0400

    first implementation

--- Day-12/project/guessing-game.py
+++ Day-12/project/guessing-game.py
@@ -12,4 +12,42 @@
 #
 # Game will end once you are out of attempts
 
+# Import random
+import random
+
+# Pick a target_num from 1 to 100
+target_num = random.randint(1, 101)
+print(f"Random num is {target_num}")
+difficulty_dict = {
+    "easy": 10,
+    "medium": 7,
+    "hard": 5,
+}
+
+# Ask user to select a difficulty
+difficulty = input("Please pick a difficulty to play on. 'Easy', 'Medium', or 'Hard':").lower()
+attempts = 0
+
+# set attempts of game based on the difficulty the user picked
+attempts = difficulty_dict[difficulty] # 10, 7, or 5
+#print(f"You have {attempts} left to guess the number.")
+
+# Run the guessing game as long as there are attempts left (loop)
+while attempts > 0:
+    print(f"You have {attempts} left to guess the number.")
+
+    # ask user to provide a guess
+    guess_num = int(input("Guess a number: "))
+
+    if guess_num == target_num:
+        print(f"Congrats you win! The number was {target_num}")
+        attempts = 0
+    elif guess_num > target_num:
+        print("Too High.")
+        print("Guess another number.")
+        attempts -= 1
+    else:
+        print("Too Low.")
+        print("Guess another number.")
+        attempts -= 1
 
