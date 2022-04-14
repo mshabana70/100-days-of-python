b651bf6ca139f61cf690c06214f1dc0ee34792f1
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-14 16:49:52 -0400

    Guess the number game overview and art

---
+++ Day-12/project/guessing-game.py
@@ -0,0 +1,15 @@
+# Day 12 Project - Number Guessing Game
+#
+# Build a game where you are to guess a number between 1 and 100 inclusive.
+# You can select a difficulty between easy, medium and hard.
+#
+# Easy, you have 10 attempts to guess the number correctly
+# Medium, you have 7 attempts to guess the number correctly
+# Hard, you have 5 attempts to guess the number correctly
+#
+# If you guess is above the target number, you will get a response of "Too High"
+# If you guess is below the target number, you will get a response of "Too Low"
+#
+# Game will end once you are out of attempts
+
+
