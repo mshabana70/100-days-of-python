cb1f449a283cf1d4c48193335e33ca3424b9f749
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-15 17:20:29 -0400

    Exercise 1 - Debug odd or even exercise

--- Day-13/exercise-1.py
+++ Day-13/exercise-1.py
@@ -3,3 +3,13 @@
 # 1. Read this the code 
 # 2. Spot the problems 🐞.
 # 3. Modify the code to fix the program.
+
+# Sample code
+number = int(input("Which number do you want to check?"))
+
+#if number % 2 = 0: # assignment operator, not comparative one
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+  
