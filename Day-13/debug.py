d6d96fd79a3e95e2bc59c452851bf43a335c870b
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-15 17:13:38 -0400

    use a breakpoint

--- Day-13/debug.py
+++ Day-13/debug.py
@@ -79,8 +79,8 @@ def mutate(a_list):
   b_list = []
   for item in a_list:
     new_item = item * 2
-    b_list.append(new_item)
-  #b_list.append(new_item)
+    b_list.append(new_item) # test with breakpoint here
+  #b_list.append(new_item) # place a breakpoint here
   print(b_list)
 
 mutate([1,2,3,5,8,13]) # Expected output: [2, 4, 6, 10, 16, 26]
