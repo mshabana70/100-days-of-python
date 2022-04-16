c04f0ffaabe23834775bc3cd61ecd9250251a064
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-16 16:54:19 -0400

    Ascii art for game start

---
+++ Day-14/project/higher_lower.py
@@ -0,0 +1,17 @@
+# Day 14 Project - Higher Lower Game
+#
+# Develop a game similar where you compare the popularity of 
+# two people. You well be comparing two randomly picked celebraties 
+# and guess which one has more followers or is more popular.
+#
+# Functionality:
+#
+# Have two randomly picked objects from an external data source (external file).
+#
+# Prompt user to pick between objects based on their popularity.
+#
+# If user is correct, continue game. If the user selects the wrong 
+# Celebrity, than end the game and return a score to the user.
+
+
+# Import random
