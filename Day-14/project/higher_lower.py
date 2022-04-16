3c84d1d29ad7b4612795e8f6a887c93b251cd649
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-16 17:11:32 -0400

    Random and compare function

--- Day-14/project/higher_lower.py
+++ Day-14/project/higher_lower.py
@@ -14,5 +14,46 @@
 # Celebrity, than end the game and return a score to the user.
 
 
-# Import random
+# Imports
 import random
+from art import logo, vs
+from game_data import data
+
+# Function to retrieve random objects
+def random_celeb():
+    celeb_list = data
+    return random.choice(celeb_list)
+
+# Compare celeb popularity (can be done before the user inputs anything)
+def compare_celeb(celeb_one, celeb_two):
+    if celeb_one["follower_count"] > celeb_two["follower_count"]:
+        return 1
+    elif celeb_one["follower_count"] < celeb_two["follower_count"]:
+        return 2
+    else:
+        return 0
+
+# Main function to prompt user to play
+def game():
+
+    score = 0
+    continue_game = True
+    print(logo)
+
+    while continue_game:
+        
+        celeb_a = random_celeb()
+        celeb_b = random_celeb()
+        compare_result = compare_celeb(celeb_a, celeb_b)
+
+        # Show user what celebs to compare
+        
+
+        user_guess = int(input("Which Celebrity is more popular? Type '1' for A, '2' for B: "))
+
+        if user_guess == compare_result:
+            score += 1
+        else:
+            print(f"That is incorrect. {")
+
+
