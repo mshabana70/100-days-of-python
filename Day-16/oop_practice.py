4e0a120b3d49788976a893da384bc30fb828b922
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-18 19:23:40 -0400

    Create screen object and access attributes

--- Day-16/oop_practice.py
+++ Day-16/oop_practice.py
@@ -1,6 +1,12 @@
 # Day 16 - Using turtle module to Practice OOP principles
 
-from turtle import Turtle
+from turtle import Turtle, Screen
 
 timmy = Turtle() # Turtle object construction 
-print(timmy)
+# print(timmy)
+
+# To access an object's attribute, you can use dot notation 
+# on the variable that holds our object
+my_screen = Screen()
+print(my_screen.canvheight) # hieght attribute of screen object
+
