ae06695fa4e5f5365a294ec990018c5321f8e2cb
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-18 19:30:51 -0400

    Using the turtle docs to move our turtle object

--- Day-16/oop_practice.py
+++ Day-16/oop_practice.py
@@ -7,6 +7,8 @@ timmy = Turtle() # Turtle object construction
 
 timmy.shape("turtle") # Turtle object method to define shape
 timmy.color("DeepSkyBlue") # Turtle object method to define color
+timmy.fd(100) # Move our turtle forward by 100 paces
+timmy.lt(100) # Move our turtle left by 100 paces
 
 # To access an object's attribute, you can use dot notation 
 # on the variable that holds our object
@@ -15,3 +17,4 @@ print(my_screen.canvheight) # hieght attribute of screen object
 
 # Accessing methods of an object, also using dot notation
 my_screen.exitonclick() # Continue running until we click on screen
+
