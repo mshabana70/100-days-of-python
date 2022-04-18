e9fb59d924b1bd65c75608d90c0b5d5df9915e52
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-18 19:39:01 -0400

    Testing PyPi by installing prettytable module

--- Day-16/oop_practice.py
+++ Day-16/oop_practice.py
@@ -2,20 +2,26 @@
 
 from turtle import Turtle, Screen
 
-timmy = Turtle() # Turtle object construction 
-# print(timmy)
+# timmy = Turtle() # Turtle object construction 
+# # print(timmy)
 
-timmy.shape("turtle") # Turtle object method to define shape
-timmy.color("DeepSkyBlue") # Turtle object method to define color
-timmy.fd(100) # Move our turtle forward by 100 paces
-timmy.lt(90) # Move our turtle to the left by 90 degrees
-timmy.fd(100) # Move our turtle forward by 100 paces
+# timmy.shape("turtle") # Turtle object method to define shape
+# timmy.color("DeepSkyBlue") # Turtle object method to define color
+# timmy.fd(100) # Move our turtle forward by 100 paces
+# timmy.lt(90) # Move our turtle to the left by 90 degrees
+# timmy.fd(100) # Move our turtle forward by 100 paces
 
-# To access an object's attribute, you can use dot notation 
-# on the variable that holds our object
-my_screen = Screen()
-print(my_screen.canvheight) # hieght attribute of screen object
+# # To access an object's attribute, you can use dot notation 
+# # on the variable that holds our object
+# my_screen = Screen()
+# print(my_screen.canvheight) # hieght attribute of screen object
 
-# Accessing methods of an object, also using dot notation
-my_screen.exitonclick() # Continue running until we click on screen
+# # Accessing methods of an object, also using dot notation
+# my_screen.exitonclick() # Continue running until we click on screen
 
+# Using PyPi (package index) to install packages in python
+
+from prettytable import PrettyTable
+
+x = PrettyTable() # Construct pretty table object
+print(x)
