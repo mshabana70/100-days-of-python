5e65c7cc943ac207e01e045b9bf4de73052dc114
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-18 19:26:41 -0400

    using methods of turtle object

--- Day-16/oop_practice.py
+++ Day-16/oop_practice.py
@@ -5,8 +5,13 @@ from turtle import Turtle, Screen
 timmy = Turtle() # Turtle object construction 
 # print(timmy)
 
+timmy.shape("turtle") # Turtle object method to define shape
+timmy.color("DeepSkyBlue") # Turtle object method to define color
+
 # To access an object's attribute, you can use dot notation 
 # on the variable that holds our object
 my_screen = Screen()
 print(my_screen.canvheight) # hieght attribute of screen object
 
+# Accessing methods of an object, also using dot notation
+my_screen.exitonclick() # Continue running until we click on screen
