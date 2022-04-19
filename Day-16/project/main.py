657ed154cae942cf1a5d0d523ba6a84a276fe15a
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-18 20:24:48 -0400

    Project requirements and docs

--- Day-16/project/main.py
+++ Day-16/project/main.py
@@ -6,3 +6,20 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+# Create objects
+coffee_machine = CoffeeMaker()
+coffee_menu = Menu()
+money_processing = MoneyMachine()
+
+# Prompt user for what they would like to drink
+user_choice = input("What would you like? (espresso/latte/cappucino)").lower()
+
+if user_choice == "off":
+    is_continue = False
+    print("Thank you for using our Coffee Machine! See you later!")
+elif user_choice == "report":
+    coffee_machine.report() # return report of current status
+else:
+    print("testing")
+
