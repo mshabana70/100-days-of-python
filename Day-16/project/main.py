19ec74fc70192c8195a3f7bf9fcf50b22b9ee49e
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-18 20:16:19 -0400

    Day 16 Project starting files

---
+++ Day-16/project/main.py
@@ -0,0 +1,8 @@
+# Day 16 Project - Coffee Machine using OOP (main code is already coded from yesterday)
+#
+# Build a program that uses external modules to recreate the coffee machine
+# program from Day 15.
+
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
