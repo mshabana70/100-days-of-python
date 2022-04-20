78d9b3e7fdeb6d01ac9bba554fd36f17b9180f11
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-20 08:19:20 -0400

    reformat data list

--- Day-17/project/main.py
+++ Day-17/project/main.py
@@ -1,9 +1,15 @@
 # Tasks:
-#
-# Create a model for our question object
-#
-# Attributes:
-## text
-## answer
-#
-# Methods:
+
+from question_model import Question
+from data import question_data
+
+# Question bank is a list of question objects
+question_bank = []
+
+for question in question_data:
+    # question text = question["text"]
+    # question answer = question["answer"]
+    # question object = Question(question["text"], question["answer"])
+    question_bank.append(Question(question["text"], question["answer"]))
+
+
