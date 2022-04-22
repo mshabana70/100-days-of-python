70cb69fc6d08393b441699769fd5776ac34b9a01
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-22 00:14:38 -0400

    listen() in turtle

--- Day-19/main.py
+++ Day-19/main.py
@@ -1,6 +1,15 @@
 # Day 19 - High Order Function, Event Listeners
 # 
-# 
+# Using turtle event listeners
+
+from turtle import Turtle
+
+tim = Turtle()
+screen = Screen()
+
+
+screen.listen()
+# Here we place an event listener
 
 
 
