6fb6321c83f088eb11d0afc34856253bbde5b54e
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-22 01:03:07 -0400

    etch a sketch app

---
+++ Day-19/challenge-1.py
@@ -0,0 +1,35 @@
+# Day 19 Challenge - Etch A Sketch App
+#
+# Create an app similar to the popular childhood game "Etch a Sketch"
+# The app should use events and listeners from the turtle module
+
+from turtle import Turtle, Screen
+
+sketch_pen = Turtle()
+screen = Screen()
+
+
+# Move functions
+def move_forward():
+    sketch_pen.forward(10)
+
+def move_right():
+    sketch_pen.right(10)
+
+def move_backwards():
+    sketch_pen.backward(10)
+
+def move_left():
+    sketch_pen.left(10)
+
+def clear_and_reset():
+    sketch_pen.clear()
+    sketch_pen.reset()
+
+screen.listen()
+screen.onkeypress(fun=move_forward, key="w")
+screen.onkeypress(fun=move_backward, key="s")
+screen.onkeypress(fun=move_right, key="d")
+screen.onkeypress(fun=move_left, key="a")
+screen.onkeypress(fun=clear_and_reset, key="c")
+screen.exitonclick()
