3ef7e200682048d47caa24f5a1255034e71fdfe4
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-22 00:24:10 -0400

    Higher order functions example

--- Day-19/main.py
+++ Day-19/main.py
@@ -18,4 +18,18 @@ screen.onkey(key="space", fun=move_forward) # listen for space key, when pressed
 # NOTE: when we pass a function as a parameter, we dont need its parenthesis
 screen.exitonclick()
 
+# Higher Order Function
+def add(n1, n2):
+    return n1 + n2
 
+def subtract(n1, n2):
+    return n1 - n2
+
+def calculator(n1, n2, func):
+    return func(n1, n2)
+
+# In this scenario, calculator is the high order function.
+# This is because calculator() is taking a function as an input,
+# and working with it in the body of the function.
+result = calculator(2, 3, add) 
+print(result)
