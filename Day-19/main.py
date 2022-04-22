831125d42e6fbac36ddeb210e5f2bcf28a97926e
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-22 00:18:24 -0400

    functions as inputs

--- Day-19/main.py
+++ Day-19/main.py
@@ -15,6 +15,7 @@ def move_forward():
 screen.listen()
 # Here we place an event listener
 screen.onkey(key="space", fun=move_forward) # listen for space key, when pressed, run move_forward function
+# NOTE: when we pass a function as a parameter, we dont need its parenthesis
 screen.exitonclick()
 
 
