13f259a1daa5bc3cc56a1fa1dc4acabcb5c78b2d
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-22 01:17:13 -0400

    Start of the turtle race project

---
+++ Day-19/project/turtle_race.py
@@ -0,0 +1,12 @@
+# Day 19 Project - Turtle Race
+#
+# Create a turtle race game where you can bet on the winner
+# prior to the race
+
+from turtle import Turtle, Screen
+
+leo = Turtle()
+screen = Screen()
+screen.setup(500, 400)
+
+screen.exitonclick()
