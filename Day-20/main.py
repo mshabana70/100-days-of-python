238f2f167925c5c0ac78dd174cc4d9bb697d74d7
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-23 17:47:52 -0400

    change code structure to OOP

--- Day-20/main.py
+++ Day-20/main.py
@@ -4,6 +4,7 @@
 # snake game.
 
 from turtle import Turtle, Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -12,21 +13,7 @@ screen.bgcolor("black")
 screen.title("Welcome to Snake Game!")
 screen.tracer(0) # turn animation off until told otherwise
 
-snake_squares = []
-starting_position = [(0, 0), (-20, 0), (-40, 0)]
-for i in range(0, 3):
-
-    # Make the visual for the square
-    square = Turtle(shape = "square")
-    square.pu()
-    square.color("white")
-
-    # Starting Position for squares 
-    #square.goto(x = starting_position[i][0], y = starting_position[i][1])
-    square.goto(starting_position[i])
-
-    # Add square to list of squares
-    snake_squares.append(square)
+snake = Snake()
 
 game_is_on = True
 while game_is_on:
@@ -35,13 +22,8 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for sq_num in range(len(snake_squares) - 1, 0, -1):
-        new_x = snake_squares[sq_num - 1].xcor()
-        new_y = snake_squares[sq_num - 1].ycor()
-        snake_squares[sq_num].goto(x = new_x, y = new_y)
+    snake.move()
     
-    snake_squares[0].forward(20)
-    snake_squares[0].left(90)
 
     
         
---
+++ Day-20/snake.py
@@ -0,0 +1,32 @@
+# Day 20 Project - Snake Game
+#
+# Class to create the snake
+from turtle import Turtle
+
+class Snake:
+
+    def __init__(self):
+        self.snake_squares = []
+        self.starting_position = [(0, 0), (-20, 0), (-40, 0)]
+        for i in range(0, 3):
+
+            # Make the visual for the square
+            self.square = Turtle(shape = "square")
+            self.square.pu()
+            self.square.color("white")
+
+            # Starting Position for squares 
+            #square.goto(x = starting_position[i][0], y = starting_position[i][1])
+            self.square.goto(self.starting_position[i])
+
+            # Add square to list of squares
+            self.snake_squares.append(self.square)
+    
+    def move(self):
+        for sq_num in range(len(self.snake_squares) - 1, 0, -1):
+            new_x = self.snake_squares[sq_num - 1].xcor()
+            new_y = self.snake_squares[sq_num - 1].ycor()
+            self.snake_squares[sq_num].goto(x = new_x, y = new_y)
+        
+        self.snake_squares[0].forward(20)
+        self.snake_squares[0].left(90)
