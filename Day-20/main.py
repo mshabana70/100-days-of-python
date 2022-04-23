8c0ccb63aa45b5d481695e5d46c322b288add890
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-23 16:47:17 -0400

    Screen setup

--- Day-20/main.py
+++ Day-20/main.py
@@ -3,3 +3,18 @@
 # We will be using the turtle module to build the classic
 # snake game.
 
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Welcome to Snake Game!")
+
+
+
+
+
+
+
+
+screen.exitonclick()
