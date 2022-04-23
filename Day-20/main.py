c721e20b1a1b71442c02dd3b7f238d2717d54b87
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-23 17:15:54 -0400

    fixing animation of snake items

--- Day-20/main.py
+++ Day-20/main.py
@@ -4,11 +4,13 @@
 # snake game.
 
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Welcome to Snake Game!")
+screen.tracer(0) # turn animation off until told otherwise
 
 snake_squares = []
 starting_position = [(0, 0), (-20, 0), (-40, 0)]
@@ -25,6 +27,16 @@ for i in range(0, 3):
 
     # Add square to list of squares
     snake_squares.append(square)
+
+game_is_on = True
+while game_is_on:
+    for square in snake_squares:
+        square.forward(20)
+        screen.update()
+        time.sleep(1)
+
+        # To clean up animation of moving snake
+
 
     
 
