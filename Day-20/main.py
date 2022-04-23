d9cc044d6fc9a8f5308d3dacd4644ddb79482736
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-23 16:59:32 -0400

    starting squares of snake game

--- Day-20/main.py
+++ Day-20/main.py
@@ -10,7 +10,18 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Welcome to Snake Game!")
 
+snake_squares = []
 
+for i in range(0, 3):
+    square = Turtle()
+    square.pu()
+    square.shape("square")
+    square.color("white")
+    snake_squares.append(square)
+
+# Starting Position for squares 
+snake_squares[1].goto(x = -20, y = 0)
+snake_squares[2].goto(x = -40, y = 0)
 
 
 
