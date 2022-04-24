eafea2969328aac1f5a090760135906c2d5f7fc7
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-24 18:27:38 -0400

    created constants

--- Day-21/project/scoreboard.py
+++ Day-21/project/scoreboard.py
@@ -1,18 +1,24 @@
 from turtle import Turtle
 
+# Constants
+ALIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
+POSITION = (0, 270)
+COLOR = "white"
+
 class Scoreboard(Turtle):
     
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.color("white")
+        self.color(COLOR)
         self.hideturtle()
         self.pu()
-        self.goto(x = 0, y = 270)
+        self.goto(POSITION)
         self.update_score()
 
     def update_score(self):
-        self.write(f"Score: {self.score}", align = "center", font = ("Arial", 24, "normal"))
+        self.write(f"Score: {self.score}", align = ALIGNMENT, font = FONT)
     
     def add_score(self):
         self.score += 1
