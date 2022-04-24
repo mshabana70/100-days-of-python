3694abf1b2f06878c4e64de87b294b5bf79c3c28
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-24 18:19:26 -0400

    Scoreboard for snake game (my implementation)

--- Day-21/project/scoreboard.py
+++ Day-21/project/scoreboard.py
@@ -8,12 +8,12 @@ class Scoreboard(Turtle):
         self.color("white")
         self.hideturtle()
         self.pu()
-        self.goto(x = 0, y = 280)
-        self.write(f"Scoreboard: 0", align = "center", font = 20)
+        self.goto(x = 0, y = 270)
+        self.write(f"Score: 0", align = "center", font = ("Arial", 24, "normal"))
 
         
     
     def add_score(self):
         self.score += 1
         self.clear()
-        self.write(f"Scoreboard: {self.score}", align = "center", font = 20)
+        self.write(f"Score: {self.score}", align = "center", font = ("Arial", 24, "normal"))
