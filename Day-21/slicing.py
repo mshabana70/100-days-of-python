ada22f78df95d298f26b6c0d74cb2edcf9b5f76b
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-24 18:50:23 -0400

    slicing in python

---
+++ Day-21/slicing.py
@@ -0,0 +1,6 @@
+# Day 21 - Practicing slicing in Python
+
+piano_keys = ["a", "b", "c", "d", "e", "f", "g"]
+ 
+print(piano_keys[2:5]) # Output: ["c", "d", "e"]
+print(piano_keys[:5]) # Output: ["a", "b", "c", "d", "e"]
