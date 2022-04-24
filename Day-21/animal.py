b84bc726e3a6a2db1ec628389c125148fdd7c9ed
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-24 15:40:15 -0400

    animal class

--- Day-21/animal.py
+++
@@ -1,1 +0,0 @@
-# Day 21 - Practicing Class Inheritance
---
+++ Day-21/inheritance.py
@@ -0,0 +1,10 @@
+# Day 21 - Practicing Class Inheritance
+class Animal:
+    def __init__(self):
+        self.num_eyes = 2
+    
+    def breathe(self):
+        print("Inhale, exhale...")
+
+
+    
