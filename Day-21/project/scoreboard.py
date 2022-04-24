8111a6a1b99aa27e9feceef6cb74eb771dfbda28
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-24 18:24:00 -0400

    refactored write function in new method update_score()

--- Day-21/project/scoreboard.py
+++ Day-21/project/scoreboard.py
@@ -9,11 +9,12 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.pu()
         self.goto(x = 0, y = 270)
-        self.write(f"Score: 0", align = "center", font = ("Arial", 24, "normal"))
+        self.update_score()
 
-        
+    def update_score(self):
+        self.write(f"Score: {self.score}", align = "center", font = ("Arial", 24, "normal"))
     
     def add_score(self):
         self.score += 1
         self.clear()
-        self.write(f"Score: {self.score}", align = "center", font = ("Arial", 24, "normal"))
+        self.update_score()
