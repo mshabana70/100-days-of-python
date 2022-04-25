85afc8b3b51e6487c47404de0a0d9b5b1e4d7f77
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-25 09:51:41 -0400

    Create paddle object

--- Day-22/project/main.py
+++ Day-22/project/main.py
@@ -3,14 +3,37 @@
 # Create the Screen
 
 from turtle import Turtle, Screen
-
+import time
 
 screen = Screen()
 screen.setup(width = 800, height = 600)
 screen.bgcolor("black")
 screen.title("Mahmoud's Pong Game")
+screen.tracer(0)
 
+# Create paddle bar 
+paddle = Turtle()
+paddle.shape("square")
+paddle.pu()
+paddle.color("white")
+paddle.shapesize(stretch_wid = 5, stretch_len = 1)
+paddle.goto(x = 350, y = 0)
 
+# Move paddle
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
 
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
