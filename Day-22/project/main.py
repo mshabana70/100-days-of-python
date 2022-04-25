5451de31127f0dabe2d3ea90c19750cef69c585f
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-25 10:00:53 -0400

    Paddle class using oop

--- Day-22/project/main.py
+++ Day-22/project/main.py
@@ -2,7 +2,8 @@
 #
 # Create the Screen
 
-from turtle import Turtle, Screen
+from turtle import Screen
+from paddle import Paddle
 import time
 
 screen = Screen()
@@ -12,25 +13,13 @@ screen.title("Mahmoud's Pong Game")
 screen.tracer(0)
 
 # Create paddle bar 
-paddle = Turtle()
-paddle.shape("square")
-paddle.pu()
-paddle.color("white")
-paddle.shapesize(stretch_wid = 5, stretch_len = 1)
-paddle.goto(x = 350, y = 0)
+paddle = Paddle()
 
-# Move paddle
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
 
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(paddle.go_up, "Up")
+screen.onkey(paddle.go_down, "Down")
 
 game_is_on = True
 while game_is_on:
--- Day-22/project/paddle.py
+++ Day-22/project/paddle.py
@@ -1,1 +1,22 @@
-# Day
+# Day 22 Project - Pong Game
+#
+# Create paddle object class
+from turtle import Turtle
+
+class Paddle(Turtle):
+
+    def __init__(self):
+        self.paddle = Turtle(shape = "square")
+        self.paddle.pu()
+        self.paddle.color("white")
+        self.paddle.shapesize(stretch_wid = 5, stretch_len = 1)
+        self.paddle.goto(x = 350, y = 0) 
+    
+    # Move paddle
+    def go_up(self):
+        new_y = self.paddle.ycor() + 20
+        self.paddle.goto(self.paddle.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.paddle.ycor() - 20
+        self.paddle.goto(self.paddle.xcor(), new_y)
