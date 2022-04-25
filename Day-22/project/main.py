fb07f98bb50a340aba4c8d8d28d0f91ad4b8129b
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-25 09:08:43 -0400

    Creating the main screen

---
+++ Day-22/project/main.py
@@ -0,0 +1,16 @@
+# Day 22 Project - Pong Game
+#
+# Create the Screen
+
+from turtle import Turtle, Screen
+
+
+screen = Screen()
+screen.setup(width = 800, height = 600)
+screen.bgcolor("black")
+screen.title("Mahmoud's Pong Game")
+
+
+
+
+screen.exitonclick()
