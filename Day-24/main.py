16d35cf3781d6d804e9d7df1e21514b2314d1fe9
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-27 13:26:31 -0400

    expanding text file

--- Day-24/main.py
+++ Day-24/main.py
@@ -1,3 +1,12 @@
 # Day 24 - Reading and writing files in python
 # 
 # 
+
+# Opening a file
+file = open("/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-24/my_file.txt") # has to be the full file path for mac i think
+
+# Read contents of file to a variable
+file_contents = file.read()
+
+# Print that variable
+print(file_contents)
