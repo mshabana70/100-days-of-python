49128741902df96a5f90c6f5005835561f9dc9e3
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-27 13:32:06 -0400

    'with' keyword to open a file

--- Day-24/main.py
+++ Day-24/main.py
@@ -13,3 +13,12 @@ print(file_contents)
 
 # Close the file to free up resources
 file.close()
+
+## Method 2 for opening a file (no need for close() using the "with" keyword)
+
+with open("/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-24/my_file.txt") as file_2:
+    # Read contents of file to a variable
+    file_contents_2 = file_2.read()
+
+    # Print that variable
+    print(file_contents_2)
