a638ae37c59e1b60aa63f2a6ba183093358bb2e4
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-27 10:34:26 -0400

    Commented out game_over() and added highscore attribute

--- Day-21/project/scoreboard.py
+++ Day-21/project/scoreboard.py
@@ -11,6 +11,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highscore = 0
         self.color(COLOR)
         self.hideturtle()
         self.pu()
@@ -20,9 +21,15 @@ class Scoreboard(Turtle):
     def update_score(self):
         self.write(f"Score: {self.score}", align = ALIGNMENT, font = FONT)
     
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", align = ALIGNMENT, font = FONT)
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+        self.score = 0
+        self.update_score()
+    
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write(f"GAME OVER", align = ALIGNMENT, font = FONT)
     
     def add_score(self):
         self.score += 1
