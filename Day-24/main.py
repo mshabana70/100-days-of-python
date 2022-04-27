631502afd7df11c5ead977a45677c102e04a3b21
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-27 13:36:17 -0400

    new file using python

--- Day-24/main.py
+++ Day-24/main.py
@@ -16,9 +16,19 @@ file.close()
 
 ## Method 2 for opening a file (no need for close() using the "with" keyword)
 
-with open("/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-24/my_file.txt") as file_2:
+with open("/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-24/my_file.txt", mode = 'w') as file_2:
     # Read contents of file to a variable
-    file_contents_2 = file_2.read()
-
+    #file_contents_2 = file_2.read()
     # Print that variable
-    print(file_contents_2)
+    #print(file_contents_2)
+
+    # To write to a file, use write() (Make sure the mode in open() is set to 'w' for write permissions)
+    file_2.write("\nNew Text from Python")
+
+
+# If file in file path does not exist, it will create the file when writing to it
+with open("/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-24/new_file.txt", mode = 'w') as file_3:
+
+    # To write to a file, use write() (Make sure the mode in open() is set to 'w' for write permissions)
+    file_3.write("\nNew Text from Python")
+
