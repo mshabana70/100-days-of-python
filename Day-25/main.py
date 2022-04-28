bba3b521d7564ee3ca226110b67286be21d6db92
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-28 17:15:53 -0400

    strip newline tag from data

--- Day-25/main.py
+++ Day-25/main.py
@@ -3,9 +3,14 @@
 # Open CSV file
 
 weather_list = []
+clean_weather_list = []
 with open("/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-25/weather_data.csv") as weather_data:
 
     # Read each line from csv file to a list
-    weather_list = weather_data.readlines()
+    weather_list = weather_data.readlines()\
 
-print(weather_list)
+
+for weather in weather_list:
+    clean_weather = weather.strip()
+    clean_weather_list.append(clean_weather)
+print(clean_weather_list)
