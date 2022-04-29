1b3d8d97456658a523ef588c951bd28e50b20d78
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-29 10:53:30 -0400

    fix state coordinates

--- Day-25/us-states-game-start/main.py
+++ Day-25/us-states-game-start/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas as pd
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -9,7 +10,10 @@ screen.addshape(img)
 turtle.shape(img)
 
 answer_state = screen.textinput(title="Guess the State:", prompt="What's the state's name?")
-print(answer_state)
+
+states_data = pd.read_csv("/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-25/us-states-game-start/50_states.csv")
+
+print(states_data[states_data["state"] == answer_state.capitalize()])
 
 # # Get the coordinate of the states with mouse clicks
 # def get_mouse_click_coor(x, y):
