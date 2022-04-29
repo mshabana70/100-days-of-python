4c0d1770c0b0bc684ab99fa1f22cb824bbaf5783
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-29 11:30:15 -0400

    Day 25 Project - U.S. State Game complete

--- Day-25/us-states-game-start/main.py
+++ Day-25/us-states-game-start/main.py
@@ -12,9 +12,9 @@ screen.addshape(img)
 turtle.shape(img)
 screen.update()
 
-game_is_going = True
+guessed_states = []
 score = 0
-while game_is_going:
+while len(guessed_states) < 50:
     
     answer_state = screen.textinput(title=f"{score}/50 states correct", prompt="What's the state's name?")
 
@@ -23,6 +23,7 @@ while game_is_going:
     if current_state.empty:
         print("It does not exist")
     else:
+        guessed_states.append(answer_state.title())
         score += 1
         turtle.pu()
         turtle.goto(x = int(current_state.x), y = int(current_state.y))
@@ -30,8 +31,11 @@ while game_is_going:
         print(f"x-coord: {int(current_state.x)}, y-coord: {int(current_state.y)}")
     
     if answer_state.lower() == "quit":
-        game_is_going = False
         print(f"Thanks for playing! Your score was {score}/50")
+        print(f"Here is the list of states you guessed right: {guessed_states}")
+        quit()
+
+print(f"Congratulations you win! Here is the order of your guesses: {guessed_states}")
 
 # # Get the coordinate of the states with mouse clicks
 # def get_mouse_click_coor(x, y):
