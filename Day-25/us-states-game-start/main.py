9ff114a35e91ceaf8da0915cf5cc1d7d78ceb96e
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-29 11:21:58 -0400

    Day 25 Project - U.S. State Game

--- Day-25/us-states-game-start/main.py
+++ Day-25/us-states-game-start/main.py
@@ -3,23 +3,41 @@ import pandas as pd
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
+screen.setup(width=750, height=500)
+screen.tracer(0)
 
 # Add image to screen as a shape
 img = "/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-25/us-states-game-start/blank_states_img.gif"
 screen.addshape(img)
 turtle.shape(img)
+screen.update()
 
-answer_state = screen.textinput(title="Guess the State:", prompt="What's the state's name?")
+game_is_going = True
+score = 0
+while game_is_going:
+    
+    answer_state = screen.textinput(title=f"{score}/50 states correct", prompt="What's the state's name?")
 
-states_data = pd.read_csv("/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-25/us-states-game-start/50_states.csv")
-
-print(states_data[states_data["state"] == answer_state.capitalize()])
+    states_data = pd.read_csv("/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-25/us-states-game-start/50_states.csv")
+    current_state = states_data[states_data["state"] == answer_state.title()]
+    if current_state.empty:
+        print("It does not exist")
+    else:
+        score += 1
+        turtle.pu()
+        turtle.goto(x = int(current_state.x), y = int(current_state.y))
+        turtle.write(answer_state.title(), font = ("Times New Roman", 10, "normal"))
+        print(f"x-coord: {int(current_state.x)}, y-coord: {int(current_state.y)}")
+    
+    if answer_state.lower() == "quit":
+        game_is_going = False
+        print(f"Thanks for playing! Your score was {score}/50")
 
 # # Get the coordinate of the states with mouse clicks
 # def get_mouse_click_coor(x, y):
 #     print(x, y)
 
 # turtle.onscreenclick(get_mouse_click_coor)
-# turtle.mainloop() # Keep screen open even after code is done executing
+turtle.mainloop() # Keep screen open even after code is done executing
 
 #screen.exitonclick()
