6a42e8deefa74f6b94fc82be235bfbe59b2c6bd1
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-29 10:37:26 -0400

    get coordinates with mouse clicks

---
+++ Day-25/us-states-game-start/main.py
@@ -0,0 +1,18 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+
+# Add image to screen as a shape
+img = "/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-25/us-states-game-start/blank_states_img.gif"
+screen.addshape(img)
+turtle.shape(img)
+
+# Get the coordinate of the states with mouse clicks
+def get_mouse_click_coor(x, y):
+    print(x, y)
+
+turtle.onscreenclick(get_mouse_click_coor)
+turtle.mainloop() # Keep screen open even after code is done executing
+
+screen.exitonclick()
