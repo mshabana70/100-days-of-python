83542a2108a0783e5341481fb283472c7075e8ed
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-30 14:44:45 -0400

    Day 26 Project - NATO alphabet second version

--- Day-26/NATO-alphabet-start/main.py
+++ Day-26/NATO-alphabet-start/main.py
@@ -11,11 +11,11 @@ import pandas as pd
 nato_df = pd.read_csv("/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-26/NATO-alphabet-start/nato_phonetic_alphabet.csv")
 
 nato_dict = {row.letter:row.code for (index, row) in nato_df.iterrows()}
-print(nato_dict)
+# print(nato_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-user_word = input("Please give us a word to translate: ")
+user_word = input("Please give us a word to translate: ").upper()
 
-result = {nato_dict[letter.upper()] for letter in user_word}
+result = {nato_dict[letter] for letter in user_word}
 print(result)
