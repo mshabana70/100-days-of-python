7a6000b1a5e22e4b99e24ac5df3ef83e41bcd777
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-30 13:53:59 -0400

    Dictionary comprehension example

--- Day-26/dict_comprehension.py
+++ Day-26/dict_comprehension.py
@@ -6,4 +6,20 @@
 # You can also create a new dict from the values of existing dictionary
 # new_dict = {new_key:new_value for (key, value) in dict.items()}
 
-# Dictionary Comp
+# Dictionary Comprehension with Conditional
+# new_dict = {new_key:new_value for (key, value) in dict.items() if conditional}
+
+# Examples
+import random
+
+names = ["Mahmoud", "Adam", "Summer", "Ahmed", "Abby", "Elsayed"]
+
+# Task: generate a dictionary where the key is a name and the value is a random score
+# assigned to each name
+students_score = {student:(random.randint(1, 100)) for student in names}
+print(students_score)
+
+# Task: Take our new generated dict and create a new dictionary with all students with a passing grade
+# (using dictionary comprehension)
+passed_students = {student:grade for (student, grade) in students_score.items() if grade > 65}
+print(passed_students)
