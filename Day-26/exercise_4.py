58be585d1b020ee6e0b1ef680d36ab4ebe5a6b92
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-30 09:44:25 -0400

    Day 26 Exercise - Dictionary Comprehension overview

---
+++ Day-26/exercise_4.py
@@ -0,0 +1,22 @@
+# Day 26 Exercise - Dictionary Comprehension 2
+#
+# ou are going to use Dictionary Comprehension to create a 
+# dictionary called result that takes each word in the given 
+# sentence and calculates the number of letters in each word.
+#
+# Try Googling to find out how to convert a sentence into a list of words.
+#
+# Example Output:
+#
+# {
+# 'What': 4, 
+# 'is': 2, 
+# 'the': 3, 
+# 'Airspeed': 8, 
+# 'Velocity': 8, 
+# 'of': 2, 
+# 'an': 2, 
+# 'Unladen': 7, 
+# 'Swallow?': 8
+# }
+
