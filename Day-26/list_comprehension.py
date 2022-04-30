9284cb1feefa31dd9e643d05cba0e37f40bf0881
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-30 08:58:48 -0400

    List comprehension with conditions examples

--- Day-26/list_comprehension.py
+++ Day-26/list_comprehension.py
@@ -31,3 +31,18 @@ print(name_list)
 
 double_list = [num * 2 for num in range(1, 5)]
 print(double_list)
+
+
+# Conditional List Comprehension
+# Keyword method: new_list = [new_item for item in list if condition]
+
+# Example
+names = ["Mahmoud", "Adam", "Summer", "Ahmed", "Abby", "Elsayed"]
+
+# List of short names for 'names' list
+short_names = [name for name in names if len(name) < 5]
+print(short_names)
+
+# Challenge: Change each of the names in 'names' to the uppercase version for names longer than 5 letters
+upper_names = [name.upper() for name in names if len(name) > 5]
+print(upper_names)
