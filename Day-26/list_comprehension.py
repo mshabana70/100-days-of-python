f18b2dbc3ba0b1732225e4c6d495edb16dbaaf7c
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-30 08:37:06 -0400

    list comprehension base file

---
+++ Day-26/list_comprehension.py
@@ -0,0 +1,14 @@
+# Day 26 - List Comprehension
+#
+
+
+# How we normally create a new list from an old one
+numbers = [1, 2, 3]
+new_list = []
+for n in numbers:
+    add_1 = n + 1
+
+new_list.append(add_1)
+print(new_list)
+
+# Using list comprehension
