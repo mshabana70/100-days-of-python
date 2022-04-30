8157a37c3bd82c4cc1b65874cf192e3281de894f
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-30 13:42:39 -0400

    dictionary comprehension keywords

---
+++ Day-26/dict_comprehension.py
@@ -0,0 +1,9 @@
+# Day 26 - Dictionary Comprehension overview
+
+# Dictionary Comprehension
+# Keyword method: new_dict = {new_key:new_value for item in list}
+
+# You can also create a new dict from the values of existing dictionary
+# new_dict = {new_key:new_value for (key, value) in dict.items()}
+
+# Dictionary Comp
