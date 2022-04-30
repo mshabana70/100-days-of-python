556c996e897cc4a29a909a63588e2ebe9617df1c
Mahmoud Shabana <mshabana5656@gmail.com>
2022-04-30 08:40:08 -0400

    Same code but using list comprehension

--- Day-26/list_comprehension.py
+++ Day-26/list_comprehension.py
@@ -7,8 +7,10 @@ numbers = [1, 2, 3]
 new_list = []
 for n in numbers:
     add_1 = n + 1
-
-new_list.append(add_1)
+    new_list.append(add_1)
 print(new_list)
 
 # Using list comprehension
+# Key word method: new_list = [new_item for item in list]
+new_list_2 = [n + 1 for n in numbers]
+print(new_list_2)
