467b0e05dca096093ed3acad23675bc5d59f4fc0
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-02 12:51:45 -0400

    using mainloop() to keep window on screen

--- Day-27/main.py
+++ Day-27/main.py
@@ -2,3 +2,10 @@
 
 # The library we will use to build a GUI will be TKinter
 import tkinter
+
+# Similar to the screen object we were creating in turtle
+window = tkinter.Tk()
+
+
+
+window.mainloop() # runs a loop to keep our window on the screen and listen for user action
