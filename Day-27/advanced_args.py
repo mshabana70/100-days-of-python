370195e50b6d3d27ac28368ad69f189414935d82
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-02 13:09:22 -0400

    key word arguments

---
+++ Day-27/advanced_args.py
@@ -0,0 +1,8 @@
+# Day 27 - Advanced Arguments in Python
+
+# Arguments with Key words
+def my_function_key(a, b, c):
+    return a + b + c
+
+# We would have to call the function like this
+my_function_key(a = 1, b = 3, c = 6)
