a356d069690666aa8aa141d75b0b76ced04773ac
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-02 13:12:26 -0400

    Functions with default values

--- Day-27/advanced_args.py
+++ Day-27/advanced_args.py
@@ -5,4 +5,15 @@ def my_function_key(a, b, c):
     return a + b + c
 
 # We would have to call the function like this
-my_function_key(a = 1, b = 3, c = 6)
+print(my_function_key(a = 1, b = 3, c = 6))
+
+# Arguments with Default Values
+def my_function_default(a = 1, b = 2, c = 3):
+    return a + b + c
+
+# We can call this function without passing any arguments
+# This is because we set default values for the arguments
+# That way we can change the arguments we need only
+print(my_function_default())
+print(my_function_default(a = 10))
+print(my_function_default(c = 12))
