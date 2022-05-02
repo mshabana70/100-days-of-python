7ce0a59ff323e8385e7b159893a2db5554f78f61
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-02 15:37:55 -0400

    Messing with button object in tkinter

--- Day-27/main.py
+++ Day-27/main.py
@@ -3,25 +3,35 @@
 # https://docs.python.org/3/library/tkinter.html#the-packer
 
 # The library we will use to build a GUI will be TKinter
-import tkinter
+from tkinter import *
 
 # Similar to the screen object we were creating in turtle
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program") # give our window a title
 window.minsize(width = 500, height = 300)
 
 
 # Label
-my_label = tkinter.Label(text = "I am a Label", font = ("Arial", 24, "bold"))
+my_label = Label(text = "I am a Label", font = ("Arial", 24, "bold"))
 
 # How will that label be displayed on screen?
 # The packer is a geometry-management mechanism
-my_label.pack(side = "left") # place it on our window and automatically center it
+#my_label.pack(side = "left") # place it on our window and automatically center it
+my_label.pack()
 
 my_label["text"] = "New Text" # will change the text property of our label
 my_label.config(text="New Text") # does the same thing to the text property
 
+# Button
 
+# Event listener for button
+def button_clicked():
+    #print("I got clicked!")
+    # Challenge: Change label text when button is clicked
+    my_label.config(text = "Button got clicked!")
+
+button = Button(text = "Click Me", command = button_clicked)
+button.pack()
 
 
 
