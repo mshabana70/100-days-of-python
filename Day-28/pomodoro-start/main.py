934c1c37a05d0c4f1969d642a3931a09de894a26
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-03 13:21:26 -0400

    Adding text to our pomodoro

--- Day-28/pomodoro-start/main.py
+++ Day-28/pomodoro-start/main.py
@@ -27,6 +27,7 @@ canvas = Canvas(width = 200, height = 224) # canvas roughly the size of our toma
 # .create_image() only takes a PhotoImage type for the image we want to display
 tomato_img = PhotoImage(file = "/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-28/pomodoro-start/tomato.png")
 canvas.create_image(103, 115, image = tomato_img) # x = 103, y = 115 to put the image in the center of the canvas
+canvas.create_text(103, 130, text = "00:00", fill = "white", font = (FONT_NAME, 35, "bold")) # place text on top of our tomato image
 canvas.pack()
 
 
