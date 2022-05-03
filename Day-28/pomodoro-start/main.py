6b6ca203ca9f54dbf0ad020f5db2e1ffb4b746ae
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-03 13:18:43 -0400

    placing tomato image on our window

--- Day-28/pomodoro-start/main.py
+++ Day-28/pomodoro-start/main.py
@@ -18,3 +18,21 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
+window.title("Pomodoro")
+window.config(padx = 100, pady = 50)
+
+# Create Canvas widget so we can layer components on one another
+canvas = Canvas(width = 200, height = 224) # canvas roughly the size of our tomato image
+
+# .create_image() only takes a PhotoImage type for the image we want to display
+tomato_img = PhotoImage(file = "/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-28/pomodoro-start/tomato.png")
+canvas.create_image(103, 115, image = tomato_img) # x = 103, y = 115 to put the image in the center of the canvas
+canvas.pack()
+
+
+
+
+
+
+
+window.mainloop()
