27881d3dae3b5b5b1525c8fed4ef31024ecae3dc
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-03 14:11:18 -0400

    Using dynamic typing for our time format

--- Day-28/pomodoro-start/main.py
+++ Day-28/pomodoro-start/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -12,21 +13,42 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(10)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    #print(count)
+
+    # Format our count to display like traditional time
+    minutes = math.floor(count / 60)
+    seconds = count % 60
+
+    # Dynamic typing
+    if seconds < 10:
+        seconds = f"0{seconds}"
+    if minutes < 10:
+        minutes = f"0{minutes}"
+
+    # decrement our timer_text as we count down every second
+    canvas.itemconfig(timer_text, text = f"{minutes}:{seconds}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Pomodoro")
 window.config(padx = 100, pady = 50, bg = YELLOW)
+
 # Create Canvas widget so we can layer components on one another
 canvas = Canvas(width = 200, height = 224, bg = YELLOW, highlightthickness = 0) # canvas roughly the size of our tomato image
 
 # .create_image() only takes a PhotoImage type for the image we want to display
 tomato_img = PhotoImage(file = "/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-28/pomodoro-start/tomato.png")
 canvas.create_image(100, 112, image = tomato_img) # x = 103, y = 112 to put the image in the center of the canvas
-canvas.create_text(100, 130, text = "00:00", fill = "white", font = (FONT_NAME, 35, "bold")) # place text on top of our tomato image
+timer_text = canvas.create_text(100, 130, text = "00:00", fill = "white", font = (FONT_NAME, 35, "bold")) # place text on top of our tomato image
 canvas.grid(column = 1, row = 1)
 
 # Labels
@@ -37,7 +59,7 @@ checkmark_label = Label(text = "✓", bg = YELLOW, fg = GREEN, font = (FONT_NAME
 checkmark_label.grid(column = 1, row = 3)
 
 # Buttons
-start_button = Button(text = "Start", bg = RED)
+start_button = Button(text = "Start", command = start_timer, bg = RED)
 start_button.grid(column = 0, row = 2)
 
 reset_button = Button(text = "Reset", bg = RED)
