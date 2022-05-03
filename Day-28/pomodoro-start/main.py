b0834824cf98d35f17b608338811c6c0a5f7309c
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-03 13:25:09 -0400

    Added color to the background of our window and canvas

--- Day-28/pomodoro-start/main.py
+++ Day-28/pomodoro-start/main.py
@@ -19,15 +19,15 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title("Pomodoro")
-window.config(padx = 100, pady = 50)
+window.config(padx = 100, pady = 50, bg = YELLOW)
 
 # Create Canvas widget so we can layer components on one another
-canvas = Canvas(width = 200, height = 224) # canvas roughly the size of our tomato image
+canvas = Canvas(width = 200, height = 224, bg = YELLOW, highlightthickness = 0) # canvas roughly the size of our tomato image
 
 # .create_image() only takes a PhotoImage type for the image we want to display
 tomato_img = PhotoImage(file = "/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-28/pomodoro-start/tomato.png")
-canvas.create_image(103, 115, image = tomato_img) # x = 103, y = 115 to put the image in the center of the canvas
-canvas.create_text(103, 130, text = "00:00", fill = "white", font = (FONT_NAME, 35, "bold")) # place text on top of our tomato image
+canvas.create_image(100, 112, image = tomato_img) # x = 103, y = 112 to put the image in the center of the canvas
+canvas.create_text(100, 130, text = "00:00", fill = "white", font = (FONT_NAME, 35, "bold")) # place text on top of our tomato image
 canvas.pack()
 
 
