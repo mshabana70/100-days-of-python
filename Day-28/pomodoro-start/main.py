62167922b2fd7af82e5a91ded9b0ef90019ee4ac
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-03 13:34:36 -0400

    Adding other widgets to pomodoro GUI

--- Day-28/pomodoro-start/main.py
+++ Day-28/pomodoro-start/main.py
@@ -28,7 +28,21 @@ canvas = Canvas(width = 200, height = 224, bg = YELLOW, highlightthickness = 0) 
 tomato_img = PhotoImage(file = "/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-28/pomodoro-start/tomato.png")
 canvas.create_image(100, 112, image = tomato_img) # x = 103, y = 112 to put the image in the center of the canvas
 canvas.create_text(100, 130, text = "00:00", fill = "white", font = (FONT_NAME, 35, "bold")) # place text on top of our tomato image
-canvas.pack()
+canvas.grid(column = 1, row = 1)
+
+# Labels
+header_label = Label(text = "Timer", bg = YELLOW, fg = GREEN, font = (FONT_NAME, 38, "normal"))
+header_label.grid(column = 1, row = 0)
+
+checkmark_label = Label(text = "✓", bg = YELLOW, fg = GREEN, font = (FONT_NAME, 25, "normal"))
+checkmark_label.grid(column = 1, row = 3)
+
+# Buttons
+start_button = Button(text = "Start", bg = YELLOW)
+start_button.grid(column = 0, row = 2)
+
+reset_button = Button(text = "Reset", bg = YELLOW)
+reset_button.grid(column = 2, row = 2)
 
 
 
