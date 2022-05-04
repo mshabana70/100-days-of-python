394e7fc38f1c2c40edd2a5d1dbe373e21540f647
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-04 11:48:05 -0400

    implemented save method

--- Day-29/password-manager-start/main.py
+++ Day-29/password-manager-start/main.py
@@ -3,6 +3,24 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    """This function saves our values from the entry objects to an external file."""
+
+    # Get entry values and store them into variables
+    website_value = website_entry.get()
+    email_value = email_entry.get()
+    password_value = password_entry.get()
+
+    # Write values to an external file
+    file = open("/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-29/password-manager-start/data.txt", "a")
+    file.write(f"{website_value} | {email_value} | {password_value}\n")
+    file.close()
+
+    # Clear existing values from entry objects (keep existing email entry)
+    website_entry.delete(0)
+    password_entry.delete(0)
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -37,7 +55,6 @@ website_entry.focus() # focus cursor on this entry on launch of app
 
 email_entry = Entry(width = 40)
 email_entry.grid(column = 1, row = 2, columnspan = 2)
-
 # END => the very last index of the entry label
 email_entry.insert(0, "mshabana5656@gmail.com") # we want our email entry to have our most common email already inputted for convenience
 
@@ -48,7 +65,7 @@ password_entry.grid(column = 1, row = 3)
 generate_button = Button(text = "Generate Password")
 generate_button.grid(column = 2, row = 3)
 
-add_button = Button(text = "Add", width = 40)
+add_button = Button(text = "Add", width = 40, command = save)
 add_button.grid(column = 1, row = 4, columnspan = 2)
 
 
