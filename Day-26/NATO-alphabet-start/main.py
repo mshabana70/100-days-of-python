1047d6fab8b9f1d51e01baf028308ce55b35d8e1
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-08 18:41:19 -0400

    Day 30 coding challenge and refactoring list comprehension

--- Day-26/NATO-alphabet-start/main.py
+++ Day-26/NATO-alphabet-start/main.py
@@ -12,22 +12,18 @@ nato_df = pd.read_csv("/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/
 
 nato_dict = {row.letter:row.code for (index, row) in nato_df.iterrows()}
 print(nato_dict)
-new_result = []
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 def generate_phonetic():
     # Day 30 coding exercise - Exception Handling of KeyError
     user_word = input("Please give us a word to translate: ").upper()
     try:
-        for letter in user_word:
-            new_result.append(nato_dict[letter])
         result = [nato_dict[letter] for letter in user_word]
     except KeyError:
         print("Sorry, only letters in the alphabet please.")
         generate_phonetic()
     else:
         print(result)
-        print(new_result)
 
 # Call new function
 generate_phonetic()
