8498ffa52da00fb01eb91ffa9e2f69aadcbecb65
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-16 13:26:21 -0400

    create image object

--- Day-31/flash-card-project-start/main.py
+++ Day-31/flash-card-project-start/main.py
@@ -6,6 +6,10 @@ window = Tk()
 window.title("Flash Card App")
 window.config(bg = BACKGROUND_COLOR)
 
+# Create Canvas for flash card image
+
+# Card image (front)
+front_card_image = PhotoImage(file = "images/card_front.png")
 
 
 window.mainloop()
