acaa64e3ec2944bc705b1eacdbdb569ccad6485c
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-16 16:33:03 -0400

    refactored button styling

--- Day-31/flash-card-project-start/main.py
+++ Day-31/flash-card-project-start/main.py
@@ -27,11 +27,11 @@ card_canvas.grid(column = 0, row = 0, columnspan=2)
 
 
 # Create button for wrong image
-wrong_button = Button(image = wrong_image, highlightcolor = BACKGROUND_COLOR, highlightthickness = 0)
+wrong_button = Button(image = wrong_image, highlightthickness = 0)
 wrong_button.grid(column = 0, row = 1)
 
 # Create button for wrong image
-right_button = Button(image = right_image, highlightcolor = BACKGROUND_COLOR, highlightthickness = 0)
+right_button = Button(image = right_image, highlightthickness = 0)
 right_button.grid(column = 1, row = 1)
 
 
