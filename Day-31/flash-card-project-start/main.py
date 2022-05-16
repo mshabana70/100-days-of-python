4e442e2c99d2f3bb7e94f2deb83cddaf3be2bd0e
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-16 15:42:36 -0400

    wrong image

--- Day-31/flash-card-project-start/main.py
+++ Day-31/flash-card-project-start/main.py
@@ -6,14 +6,17 @@ window = Tk()
 window.title("Flash Card App")
 window.config(padx = 50, pady = 50, bg = BACKGROUND_COLOR)
 
-# Create Canvas for flash card image
-card_canvas = Canvas(width = 800, height = 526)
+
 
 # Card image (front)
 front_card_image = PhotoImage(file = "/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-31/flash-card-project-start/images/card_front.png")
 
+# Create Canvas for flash card image
+card_canvas = Canvas(width = 800, height = 526)
+
 # Insert card_image to canvas using create image
 card_canvas.create_image(400, 250, image = front_card_image)
+card_canvas.grid(column = 0, row = 0, columnspan=2)
 
 
 window.mainloop()
