877416b5e5d72c40238a930335d7b7041f164822
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-16 13:30:24 -0400

    testing card image on canvas

--- Day-31/flash-card-project-start/main.py
+++ Day-31/flash-card-project-start/main.py
@@ -7,9 +7,14 @@ window.title("Flash Card App")
 window.config(bg = BACKGROUND_COLOR)
 
 # Create Canvas for flash card image
+card_canvas = Canvas(width = 800, height = 526)
+card_canvas.config(padx = 50, pady = 50)
 
 # Card image (front)
 front_card_image = PhotoImage(file = "images/card_front.png")
+
+# Insert card_image to canvas
+card_canvas.create_image(400, 250, image = front_card_image)
 
 
 window.mainloop()
