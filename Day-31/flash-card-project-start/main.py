571f20c6668a726a67239d4cd38f07cb4a3ede69
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-16 15:42:52 -0400

    wrong canvas and right image

--- Day-31/flash-card-project-start/main.py
+++ Day-31/flash-card-project-start/main.py
@@ -10,6 +10,8 @@ window.config(padx = 50, pady = 50, bg = BACKGROUND_COLOR)
 
 # Card image (front)
 front_card_image = PhotoImage(file = "/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-31/flash-card-project-start/images/card_front.png")
+wrong_image = PhotoImage(file = "/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-31/flash-card-project-start/images/wrong.png")
+right_image = PhotoImage(file = "/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-31/flash-card-project-start/images/right.png")
 
 # Create Canvas for flash card image
 card_canvas = Canvas(width = 800, height = 526)
@@ -17,6 +19,13 @@ card_canvas = Canvas(width = 800, height = 526)
 # Insert card_image to canvas using create image
 card_canvas.create_image(400, 250, image = front_card_image)
 card_canvas.grid(column = 0, row = 0, columnspan=2)
+
+# Create canvas for wrong image
+wrong_canvas = Canvas(width = 400, height = 50)
+wrong_canvas.create_image(200, 25, image = wrong_image)
+wrong_canvas.grid(column = 0, row = 1)
+
+
 
 
 window.mainloop()
