5806b3c0788483687de936d687acb89800703fc8
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-16 15:48:12 -0400

    images and canvas layouts complete, next is text

--- Day-31/flash-card-project-start/main.py
+++ Day-31/flash-card-project-start/main.py
@@ -14,18 +14,21 @@ wrong_image = PhotoImage(file = "/Users/mahmoudshabana/Documents/Udemy/100-days-
 right_image = PhotoImage(file = "/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-31/flash-card-project-start/images/right.png")
 
 # Create Canvas for flash card image
-card_canvas = Canvas(width = 800, height = 526)
+card_canvas = Canvas(width = 800, height = 526, bg = BACKGROUND_COLOR, highlightthickness= 0)
 
 # Insert card_image to canvas using create image
-card_canvas.create_image(400, 250, image = front_card_image)
+card_canvas.create_image(400, 265, image = front_card_image)
 card_canvas.grid(column = 0, row = 0, columnspan=2)
 
 # Create canvas for wrong image
-wrong_canvas = Canvas(width = 400, height = 50)
-wrong_canvas.create_image(200, 25, image = wrong_image)
+wrong_canvas = Canvas(width = 400, height = 100, bg = BACKGROUND_COLOR, highlightthickness= 0)
+wrong_canvas.create_image(200, 52, image = wrong_image)
 wrong_canvas.grid(column = 0, row = 1)
 
-
+# Create canvas for right image
+right_canvas = Canvas(width = 400, height = 100, bg = BACKGROUND_COLOR, highlightthickness= 0)
+right_canvas.create_image(200, 52, image = right_image)
+right_canvas.grid(column = 1, row = 1)
 
 
 window.mainloop()
