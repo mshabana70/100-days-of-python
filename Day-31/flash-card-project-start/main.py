aa1335c7115f28b9dde50afca9ff78a91399fb24
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-16 13:33:28 -0400

    refactored file paths

--- Day-31/flash-card-project-start/main.py
+++ Day-31/flash-card-project-start/main.py
@@ -10,9 +10,9 @@ window.config(padx = 50, pady = 50, bg = BACKGROUND_COLOR)
 card_canvas = Canvas(width = 800, height = 526)
 
 # Card image (front)
-front_card_image = PhotoImage(file = "images/card_front.png")
+front_card_image = PhotoImage(file = "/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-31/flash-card-project-start/images/card_front.png")
 
-# Insert card_image to canvas
+# Insert card_image to canvas using create image
 card_canvas.create_image(400, 250, image = front_card_image)
 
 
