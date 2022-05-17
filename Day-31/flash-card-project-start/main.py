dbd4dc1a4aa7f5e22584477802141fceb9f7d7e7
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-17 15:51:41 -0400

    removed get_words func

--- Day-31/flash-card-project-start/main.py
+++ Day-31/flash-card-project-start/main.py
@@ -3,46 +3,32 @@ import pandas as pd
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-timer = None
-count = 0
-
 
 ####################### READ DATA FROM CSV ######################
-def get_words():
-    word_data = pd.read_csv("/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-31/flash-card-project-start/data/french_words.csv")
-
-    list_of_words = []
-    list_of_words = word_data.to_dict('records')
-    return list_of_words
+word_data = pd.read_csv("/Users/mahmoudshabana/Documents/Udemy/100-days-of-python/Day-31/flash-card-project-start/data/french_words.csv")
+list_of_words = []
+list_of_words = word_data.to_dict('records')
+current_card = {}
 
 ####################### DISPLAY WORDS FROM DATA ######################
 def next_card():
-    list_of_words = get_words()
-    word = random.choice(list_of_words)
-
+    global current_card
+    current_card = random.choice(list_of_words)
+    card_canvas.itemconfig(title_text, text = "French")
     card_canvas.itemconfig(word_text, text = word["French"])
 
 def flip_card():
     # Change image of card
     card_canvas.itemconfig(card_image, image = back_card_image)
-
     # Change text
     card_canvas.itemconfig(title_text, fill = "white", text = "English")
-    card_canvas.itemconfig(word_text, fill = "white", text = )
-
-
-def timer_count():
-    if count < 3:
-        timer = window.after(1000, timer_count, count + 1)
-    else:
-        flip_card()
-
-
+    card_canvas.itemconfig(word_text, fill = "white", text = current_card["English"])
 
 
 window = Tk()
 window.title("Flash Card App")
 window.config(padx = 50, pady = 50, bg = BACKGROUND_COLOR)
+window.after(3000, func = flip_card)
 
 
 # Card image (front)
