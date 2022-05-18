15c10e8db5b3be7afb50f2876b3a1365502694bc
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-18 12:11:36 -0400

    fixing error in smtp using port number

--- Day-32/main.py
+++ Day-32/main.py
@@ -3,8 +3,11 @@ import smtplib
 my_email = "moonshabana70@gmail.com"
 password = "test123forprogram"
 
-connection = smtplib.SMTP("smtp.gmail.com")
+connection = smtplib.SMTP("smtp.gmail.com", port=587)
 connection.starttls() # Start transport layer security (encrypts email in transit)
 connection.login(user=my_email, password= password)
-connection.sendmail(from_addr= my_email, to_addrs= "moonshabana70@yahoo.com", msg= "Hello World!")
+connection.sendmail(
+    from_addr= my_email,
+    to_addrs= "moonshabana70@yahoo.com", 
+    msg= "Subject:Hello World!\n\nThis is the body of my email.")
 connection.close()
