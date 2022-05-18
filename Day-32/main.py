36e1931082750b24000281a43e7e4f95a133dfb4
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-18 11:58:34 -0400

    sending email to another user

--- Day-32/main.py
+++ Day-32/main.py
@@ -1,3 +1,10 @@
 import smtplib
 
+my_email = "moonshabana70@gmail.com"
+password = "test123forprogram"
+
 connection = smtplib.SMTP("smtp.gmail.com")
+connection.starttls() # Start transport layer security (encrypts email in transit)
+connection.login(user=my_email, password= password)
+connection.sendmail(from_addr= my_email, to_addrs= "moonshabana70@yahoo.com", msg= "Hello World!")
+connection.close()
