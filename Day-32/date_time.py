7567d2833c5926d63f2d0f4b89d44090972c5a19
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-18 14:06:18 -0400

    create our own datetime object (DOB)

--- Day-32/date_time.py
+++ Day-32/date_time.py
@@ -2,4 +2,10 @@
 import datetime as dt
 
 now = dt.datetime.now()
-print(now)
+year = now.year
+month = now.month
+day_of_week = now.weekday() # returns an int
+print(day_of_week)
+
+date_of_birth = dt.datetime(year = 1998, month = 7, day = 14, hour = 6)
+print(date_of_birth)
