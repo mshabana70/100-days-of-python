01cd52a0b4875ae7e5fcfa18d15587751bc09777
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-19 11:13:25 -0400

    quick note on response codes

--- Day-33/main.py
+++ Day-33/main.py
@@ -1,4 +1,12 @@
 # Making an API Request in python
+
+# Response Codes
+# 1XX: Hold On (still processing)
+# 2XX: Here you go (successful)
+# 3XX: Go Away (lack permissions)
+# 4XX: You screwed up (Request does not exist)
+# 5XX: I screwed up (Error on the server's side)
+
 import requests
 
 response = requests.get(url= "http://api.open-notify.org/iss-now.json")
