620915931142bac13a49c8236a643487e614a07d
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-20 12:15:45 -0400

    Working on Quizzler UI

--- Day-34/quizzler-app-start/ui.py
+++ Day-34/quizzler-app-start/ui.py
@@ -1,2 +1,28 @@
-THEME_COLOR = "#375362"
+from tkinter import *
 
+THEME_COLOR = "#375362"
+FONT = ("Arial", 20, "italic")
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler App")
+
+        # Create padding and bg color of window
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        # Create score label
+        self.score = 0
+        self.score_label = Label(text=f"Score: {self.score}", fg="white", bg=THEME_COLOR, highlightthickness=0)
+        self.score_label.grid(row=0, column=1)
+
+        # Create canvas
+        self.canvas = Canvas(width=300, height=250)
+        self.question_text = self.canvas.create_text(150, 175, text="SAMPLE QUESTION TEXT GOES HERE", width=270, font=FONT, fill=THEME_COLOR)
+
+
+
+
+
+        self.window.mainloop()
