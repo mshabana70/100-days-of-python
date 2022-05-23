29e6a9077551156bdf2e0ff66658a98c4bfecfbf
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-23 15:56:46 -0400

    API Key

--- Day-35/main.py
+++ Day-35/main.py
@@ -5,4 +5,6 @@
 # For example: a weather api might charge users depending on the load 
 # of calls that the weather provider recieves using their API.
 #
-# Think of these paid APIs as a way of selling computed data.
+# Think of these paid APIs as a way of selling computed data. A way 
+# to keep users from gaining access to this data for free is by using
+# a "API Key".
