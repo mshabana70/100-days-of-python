cf82af4ed7602983ae3b2a7a265ef690a127e91f
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-23 15:51:31 -0400

    API Authentication

--- Day-35/main.py
+++ Day-35/main.py
@@ -1,4 +1,5 @@
 # Day 35 - Keys, Authentication and Environment Variables: Send sms
 #
 # The APIs used prior were free so there wasnt much of a need to 
-# authenticate 
+# authenticate the user.
+# 
