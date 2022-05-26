94a3b65bc0fe9c05612f73fa4b862331e11f9870
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-26 09:50:28 -0400

    stock api key

--- Day-36/stock-news-extrahard-start/main.py
+++ Day-36/stock-news-extrahard-start/main.py
@@ -1,5 +1,6 @@
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+STOCK_API_KEY = "D3TOUPNM675LM8QI"
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
