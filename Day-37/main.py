253ba296bfb6179da190ae0c81595219155dc274
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-27 11:15:17 -0400

    HTTP Requests (post)

--- Day-37/main.py
+++ Day-37/main.py
@@ -1,6 +1,6 @@
 # HTTP Requests
 #
 # GET (requests.get()): get data or information using this request
-# POST 
-#
+# POST (requests.post()): give data to external system and check if it was successful
+# PUT (requests.put()): 
 # 
