03d0ee5c5c3e84292b328212f4cd35191fa55a20
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-28 19:01:58 -0400

    pixela link

--- Day-37/main.py
+++ Day-37/main.py
@@ -1,5 +1,6 @@
 # HTTP Requests
-
+#
+# https://pixe.la/
 #
 # GET (requests.get()): get data or information using this request
 # POST (requests.post()): give data to external system and check if it was successful
