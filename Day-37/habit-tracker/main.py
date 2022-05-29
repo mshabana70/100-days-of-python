af821ac7092bb37334e21f8b6b83c9ba064e6362
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-29 13:25:00 -0400

    user parameters

--- Day-37/habit-tracker/main.py
+++ Day-37/habit-tracker/main.py
@@ -2,4 +2,11 @@ import requests
 
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 
+user_params = {
+    "token": "isjaogoy328o3896sgh",
+    "username": "mahmoud",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
 requests.post()
