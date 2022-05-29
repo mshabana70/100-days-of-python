84f5b3151bc9aace85539746fe5d660282ec22f1
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-29 13:28:38 -0400

    created pixela user

--- Day-37/habit-tracker/main.py
+++ Day-37/habit-tracker/main.py
@@ -4,10 +4,12 @@ PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 
 user_params = {
     "token": "isjaogoy328o3896sgh",
-    "username": "mahmoud",
+    "username": "mshabana70",
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
-# passing in json data for user parameters
-requests.post(url=PIXELA_ENDPOINT, json=user_params)
+# Passing in json data for user parameters 
+# Comment out code below once user is created
+# response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+# print(response.text)
