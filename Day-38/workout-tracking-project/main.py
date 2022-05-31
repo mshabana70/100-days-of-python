3fbcdc4db88a2cbbcbd898cade614ade769506bf
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-31 12:07:08 -0400

    Day 38 Project - Workout Tracker complete

--- Day-38/workout-tracking-project/main.py
+++ Day-38/workout-tracking-project/main.py
@@ -6,9 +6,11 @@ from datetime import datetime
 # APP CONSTANTS
 WORKOUT_APP_ID = os.environ.get("WORKOUT_APP_ID")
 WORKOUT_API_KEY = os.environ.get("WORKOUT_API_KEY")
+SHEETY_TOKEN = os.environ.get("SHEETY_TOKEN")
+sheety_endpoint = os.environ.get("SHEETY_ENDPOINT")
 
 
-# POST Request
+# Exercise POST Request
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
 request_headers = {
@@ -47,14 +49,35 @@ current_time = current.strftime("%H:%M:%S")
 # print(current_date)
 # print(current_time)
 
+# Sheety POST Request
+
+sheety_headers = {
+    "Authorization": f"Bearer {SHEETY_TOKEN}"
+}
+
 # for every exercise:
 for exercise in exercise_data:
 
     # Format data for spreadsheet
-    exercise_name = exercise["name"]
+    exercise_name = exercise["name"].title()
     exercise_duration = exercise["duration_min"]
     exercise_calories = exercise["nf_calories"]
 
+    # set request for sheety with exercise and datetime data
     #print(f"Name: {exercise_name}, duration: {exercise_duration}, calories burned: {exercise_calories}")
+
+    sheety_body = {
+        "workout": {
+            "date": current_date,
+            "time": current_time,
+            "exercise": exercise_name,
+            "duration": exercise_duration,
+            "calories": exercise_calories,
+        }
+    }
+
+    sheety_response = requests.post(url=sheety_endpoint, json=sheety_body, headers=sheety_headers)
+    print(sheety_response.raise_for_status())
+    
 
 
