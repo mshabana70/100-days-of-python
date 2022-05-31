c40ded157e2278cb3f947fbe3289a6761312fbaf
Mahmoud Shabana <mshabana5656@gmail.com>
2022-05-31 10:38:59 -0400

    request header

--- Day-38/workout-tracking-project/main.py
+++ Day-38/workout-tracking-project/main.py
@@ -2,5 +2,15 @@
 import requests
 import os
 
-APP_ID = "cbf58515"
+WORKOUT_APP_ID = "cbf58515"
 WORKOUT_API_KEY = os.environ(WORKOUT_API_KEY)
+
+request_headers = {
+    "x-app-id": WORKOUT_APP_ID,
+    "x-app-key": WORKOUT_API_KEY,
+    "x-remote-user-id": 0,
+}
+
+request_body = {
+    
+}
