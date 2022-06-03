8075b6eca56f038e3e4318448d0334b6c643f5c9
Mahmoud Shabana <mshabana5656@gmail.com>
2022-06-03 16:25:14 -0400

    work on project soon

--- Day-39/flight-deals-start/main.py
+++ Day-39/flight-deals-start/main.py
@@ -1,2 +1,2 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
-# Need to finish this project this 
+# Need to finish this project this weekend!!!
