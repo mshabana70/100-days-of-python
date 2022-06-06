45028947ae7f82d52b96e70595dcb1d77a31d79d
Mahmoud Shabana <mshabana5656@gmail.com>
2022-06-06 18:32:40 -0400

    Flight url

--- Day-39/flight-deals-start/flight_search.py
+++ Day-39/flight-deals-start/flight_search.py
@@ -1,9 +1,11 @@
 import requests
+import os
 
-FLIGHT_URL = ""
+FLIGHT_URL = "https://tequila-api.kiwi.com/v2/search"
+
 
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-
+    
 
     pass
