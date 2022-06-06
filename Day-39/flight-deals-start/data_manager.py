0a03e4d8704e837cd62d2c1af89d7c85519242ae
Mahmoud Shabana <mshabana5656@gmail.com>
2022-06-06 18:39:53 -0400

    sheety url and token

--- Day-39/flight-deals-start/data_manager.py
+++ Day-39/flight-deals-start/data_manager.py
@@ -1,5 +1,11 @@
 import requests
+import os
+
+SHEETY_ENDPOINT = os.environ.get("SHEETY_ENDPOINT")
+SHEETY_AUTH = os.environ.get("SHEETY_AUTH")
 
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
+
+    
     pass
