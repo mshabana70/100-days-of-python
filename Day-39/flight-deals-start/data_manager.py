867756312afda0d0070e8482b878da3e6d01af35
Mahmoud Shabana <mshabana5656@gmail.com>
2022-06-06 18:56:59 -0400

    testing datamanager

--- Day-39/flight-deals-start/data_manager.py
+++ Day-39/flight-deals-start/data_manager.py
@@ -4,8 +4,23 @@ import os
 SHEETY_ENDPOINT = os.environ.get("SHEETY_ENDPOINT")
 SHEETY_AUTH = os.environ.get("SHEETY_AUTH")
 
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
 
+    def __init__(self):
+        self.endpoint = SHEETY_ENDPOINT
+        self.sheety_header = {
+            "Authorization": f"Bearer {SHEETY_AUTH}"
+        }
+        self.response = ""
     
+    def getCities(self):
+        self.response = requests.request('GET', url=self.endpoint, headers=self.sheety_header)
+        return self.response.text
+
+
     pass
+
+dm = DataManager()
+print(dm.getCities())
