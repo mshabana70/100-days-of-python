b4a5d5e1232fc2a7827486175862f04f78bda941
Mahmoud Shabana <mshabana5656@gmail.com>
2022-06-06 19:22:47 -0400

    get IATA codes using flight location endpoint'

--- Day-39/flight-deals-start/data_manager.py
+++ Day-39/flight-deals-start/data_manager.py
@@ -17,12 +17,23 @@ class DataManager:
         self.response = ""
     
     def get_data(self):
-        self.response = requests.request('GET', url=self.endpoint, headers=self.sheety_header)
+        '''Get all records in our Flight sheet'''
+        self.response = requests.get(url=self.endpoint, headers=self.sheety_header)
         return self.response.json()
     
     def get_IATA(self):
+        '''Returns the IATA of each city in our sheets'''
+        cities = self.get_data()["prices"]
+        location_endpoint = "https://tequila-api.kiwi.com/locations/anything"
+        for city in cities:
+            name_of_city = city["city"]
+            flight_parameters = {
+                "apikey": FLIGHT_AUTH,
+                "key": "name",
+                "value": name_of_city
+            }
         
-
-
+        self.response = request.post(url=location_endpoint, params=flight_parameters)
+        return self.response.json()
 
     pass
