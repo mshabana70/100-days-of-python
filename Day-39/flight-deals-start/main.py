5340af900bbfbfe20bda6a44defa2b581fdc181c
Mahmoud Shabana <mshabana5656@gmail.com>
2022-06-08 19:29:13 -0400

    calling dataManager in main

--- Day-39/flight-deals-start/main.py
+++ Day-39/flight-deals-start/main.py
@@ -1,2 +1,9 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
-# 
+
+from data_manager import DataManager
+
+
+dt = DataManager()
+
+sheet_data = dt.get_data()
+print(sheet_data)
