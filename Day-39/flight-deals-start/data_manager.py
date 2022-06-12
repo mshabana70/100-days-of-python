8ee00bee426a9c95eb84cc89744d5deb53f209a3
Mahmoud Shabana <mshabana5656@gmail.com>
2022-06-11 21:00:57 -0400

    cleaning up data_manager'

--- Day-39/flight-deals-start/data_manager.py
+++ Day-39/flight-deals-start/data_manager.py
@@ -34,20 +34,6 @@ class DataManager:
             print(response.text
 
     
-    # def get_IATA(self):
-    #     '''Returns the IATA of each city in our sheets'''
-    #     cities = self.get_data()["prices"]
-    #     location_endpoint = "https://tequila-api.kiwi.com/locations/anything"
-    #     for city in cities:
-    #         name_of_city = city["city"]
-    #         flight_parameters = {
-    #             "apikey": FLIGHT_AUTH,
-    #             "key": "name",
-    #             "value": name_of_city
-    #         }
-        
-    #     self.response = request.post(url=location_endpoint, params=flight_parameters)
-    #     return self.response.json()
     
 
 
