3783b9d824f14cb2831dcf243f66817507a1603b
Mahmoud Shabana <mshabana5656@gmail.com>
2022-06-15 21:07:05 -0400

    formatting departure dates for flight using datetime

--- Day-39/flight-deals-start/main.py
+++ Day-39/flight-deals-start/main.py
@@ -2,18 +2,31 @@
 
 from data_manager import DataManager
 from flight_search import FlightSearch
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # Get Data from our google spread sheets
 sheet_data = DataManager()
 fs = FlightSearch()
 sheet_data.get_data()
 
+now_date = datetime.now()
+six_months_date = now_date + timedelta(days=183)
+
+flight_date_from = now_date.strftime("%m/%d/%Y")
+flight_date_to = six_months_date.strftime("%m/%d/%Y")
+
 # Check if IATA codes exist in our data
 for city in sheet_data.data:
     if city["iataCode"] != "":
-        # Parse through city codes to get flight pricing 
-        print(city)
+        # Parse through city codes to get flight pricing
+        flight_price = fs.get_flight_price(
+            dest_city=city["city"], 
+            date_from=, 
+            date_to, 
+            return_date_from, 
+            return_date_to
+        ) 
+        print(flight_price)
     else:
         iataCode = fs.get_IATA(city["city"])
         city["iataCode"] = iataCode
