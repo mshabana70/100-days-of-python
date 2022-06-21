9b40b37199ef72121e33d9ed663bbec1f393fe36
Mahmoud Shabana <mshabana5656@gmail.com>
2022-06-21 19:58:14 -0400

    attribute for flightData

--- Day-39/flight-deals-start/flight_data.py
+++ Day-39/flight-deals-start/flight_data.py
@@ -1,5 +1,5 @@
 class FlightData:
     #This class is responsible for structuring the flight data.
-    def __init__(self)
+    def __init__(self, price, origin_city, origin_airport, destination_city, destination_airport, out_date, return_date):
 
     pass
