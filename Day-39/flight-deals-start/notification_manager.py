46903554a3d53644a199d48c31c8f29c662ac2e4
Mahmoud Shabana <mshabana5656@gmail.com>
2022-06-22 17:55:48 -0400

    send_sms complete

--- Day-39/flight-deals-start/notification_manager.py
+++ Day-39/flight-deals-start/notification_manager.py
@@ -11,7 +11,11 @@ class NotificationManager:
         self.recepient_num = "+16464278840"
 
     def send_sms(self, price, departure_city, departure_code, arrival_city, arrival_code, out_date, in_date):
-        body = f""
+        body = f"Low Price Alert! Only ${price} to fly from {departure_city}-{departure_code} to {arrival_city}-{arrival_code}, from {out_date} to {in_date}."
         client = Client(API_SID, API_KEY)
-
-    pass
+        message = client.messages.create(
+            body=body,
+            from_=self.sender_num,
+            to=self.recepient_num
+        )
+        print(message.status)
