611dc343cb36d2001b9ede5e657f3acdb1c59cb3
Mahmoud Shabana <mshabana5656@gmail.com>
2022-06-22 17:45:30 -0400

    starting constructor

--- Day-39/flight-deals-start/notification_manager.py
+++ Day-39/flight-deals-start/notification_manager.py
@@ -6,5 +6,6 @@ API_KEY = os.environ.get("TWILIO_KEY")
 
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
-
+    def __init__(self):
+        
     pass
