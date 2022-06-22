1863cf4729210eda2462197a6c9ded92d2bbf396
Mahmoud Shabana <mshabana5656@gmail.com>
2022-06-22 17:50:45 -0400

    send_sms() method

--- Day-39/flight-deals-start/notification_manager.py
+++ Day-39/flight-deals-start/notification_manager.py
@@ -9,4 +9,8 @@ class NotificationManager:
     def __init__(self):
         self.sender_num = "+12058582732"
         self.recepient_num = "+16464278840"
+        self.body = ""
+
+    def send_sms(self, price, departure_city, departure_code, arrival_city, arrival_code, out_date, in_date):
+             
     pass
