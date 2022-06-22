9b80e67e9999756e17a1314222baa79ab94974fb
Mahmoud Shabana <mshabana5656@gmail.com>
2022-06-22 17:46:52 -0400

    properties

--- Day-39/flight-deals-start/notification_manager.py
+++ Day-39/flight-deals-start/notification_manager.py
@@ -7,5 +7,6 @@ API_KEY = os.environ.get("TWILIO_KEY")
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
     def __init__(self):
-        
+        self.sender_num = "+12058582732"
+        self.recepient_num = "+16464278840"
     pass
