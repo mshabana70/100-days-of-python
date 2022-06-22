39aaf53da045288b9480450467e6106d3ae80a54
Mahmoud Shabana <mshabana5656@gmail.com>
2022-06-22 17:44:48 -0400

    Add values using environmental variables

--- Day-39/flight-deals-start/notification_manager.py
+++ Day-39/flight-deals-start/notification_manager.py
@@ -1,9 +1,8 @@
 from twilio import Client
 import os
 
-TWILIO_ENDPOINT = ""
-API_SID = ""
-API_KEY = ""
+API_SID = os.environ.get("TWILIO_SID")
+API_KEY = os.environ.get("TWILIO_KEY")
 
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
