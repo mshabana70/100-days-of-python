90ef3b1202fde55deae99716ad9510dd3d3a9e64
Mahmoud Shabana <mshabana5656@gmail.com>
2022-06-25 10:55:50 -0400

    fix spacing

--- Day-40/main.py
+++ Day-40/main.py
@@ -3,4 +3,5 @@ import requests
 
 # Starting console print
 print("Welcome to Mahmoud's Flight Club!\nWe find the best flight deals and email it to you.")
+
 first_name = input("What is your first name?\n").title()
