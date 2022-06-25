a994c8b67fca763d96350f587e8083ef7d524b9b
Mahmoud Shabana <mshabana5656@gmail.com>
2022-06-25 11:05:11 -0400

    post request for sheety

--- Day-40/main.py
+++ Day-40/main.py
@@ -8,10 +8,11 @@ first_name = input("What is your first name?\n").capitalize()
 last_name = input("What is your last name?\n").capitalize()
 
 does_match = False
-email = input("what is your email?")
+email = input("what is your email?\n")
 while not (does_match):
-  validate_email = input("Type your email again.")
+  validate_email = input("Type your email again.\n")
   if validate_email == email:
     does_match = True
   else:
     print("Sorry! the email does not match.")
+
