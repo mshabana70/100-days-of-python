44e39bf049c91ca6543d41e12b2b58166b636da3
Mahmoud Shabana <mshabana5656@gmail.com>
2022-06-25 10:54:59 -0400

    starting console program

--- Day-40/main.py
+++ Day-40/main.py
@@ -1,1 +1,5 @@
 # Starting File for Part 2 of Flight deals project
+import requests
+
+# Starting console print
+print("Welcome to Mahmoud's Flight Club!\nWe find the best flight deals and email it to you.")
