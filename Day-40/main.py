39ad3bfb6303708788f39fae8ce3cb44b7504bd1
Mahmoud Shabana <mshabana5656@gmail.com>
2022-06-25 10:56:39 -0400

    capitalize function

--- Day-40/main.py
+++ Day-40/main.py
@@ -4,4 +4,5 @@ import requests
 # Starting console print
 print("Welcome to Mahmoud's Flight Club!\nWe find the best flight deals and email it to you.")
 
-first_name = input("What is your first name?\n").title()
+first_name = input("What is your first name?\n").capitalize()
+last_name = input("What is your last name?\n").capitalize()
