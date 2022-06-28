170fee8f5a9f67948af7007ad0452bf075032321
Mahmoud Shabana <mshabana5656@gmail.com>
2022-06-28 10:56:18 -0400

    smtp logic

--- Day-40/flight-deals-part-2/flight-deals-start/notification_manager.py
+++ Day-40/flight-deals-part-2/flight-deals-start/notification_manager.py
@@ -1,8 +1,11 @@
 from twilio.rest import Client
 import os
+import smtplib
 
 API_SID = os.environ.get("TWILIO_SID")
 API_KEY = os.environ.get("TWILIO_KEY")
+MY_EMAIL = os.environ.get("MY_EMAIL")
+MY_EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
@@ -18,3 +21,14 @@ class NotificationManager:
             to=self.recepient_num
         )
         print(message.status)
+
+    def send_email(self, emails, message, link):
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(email=MY_EMAIL, password=MY_EMAIL_PASSWORD)
+            for email in emails:
+                connection.sendmail(
+                    from_addr=MY_EMAIL,
+                    to_addrs=email,
+                    msg=f"Subject:New Low Price Flight!\n\n{message}\n{google_flight_link}".encode('utf-8')
+                )
